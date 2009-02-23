05a5fb12f0de81590dd19c0d6cac6f5f0d46bf43
Adam Mckaig <adam.mckaig@gmail.com>
2009-02-24 05:26:48 +0800

    replaced tabs with spaces
    Signed-off-by: unicefinnovation <amckaig@unicef.org>

--- alpha/app.py
+++ alpha/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-# vim: noet
+# vim: ai ts=4 sts=4 et sw=4
 
 import rapidsms
 
 # a pointless app to demonstrate the structure
 # of sms applications without magic decorators
 class App(rapidsms.app.Base):
-	def incoming(msg):
-		msg.respond("Alpha!")
+    def incoming(msg):
+        msg.respond("Alpha!")
