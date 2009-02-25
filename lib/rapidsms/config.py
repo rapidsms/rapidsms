6761ca01bbf96077324eae8f5047212ff428c63f
Mark E Johnston <mejymejy@gmail.com>
2009-02-25 17:21:19 -0500

    added backends to config and backend script

--- lib/rapidsms/config.py
+++ lib/rapidsms/config.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import json
+from __future__ import with_statement
+import simplejson
 
 class Config():
-    def __init__(self, path):
 
+    def __init__(self, path):
         # load the config, and parse it. i chose json because
         # it's in the python stdlib and is language-neutral
         with open(path) as f:
             self.raw = f.read()
-            self.data = json.loads(self.raw)
+            self.data = simplejson.loads(self.raw)
 
     def __getitem__(self, items):
         return self.data[items]
