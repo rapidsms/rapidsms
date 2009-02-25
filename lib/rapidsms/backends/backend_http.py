4c7e392251bcd02f591bfae12ea894d410eb0e80
Mark E Johnston <mejymejy@gmail.com>
2009-02-25 18:19:54 -0500

    implemented VERY basic Http backend

--- lib/rapidsms/backends/backend_http.py
+++ lib/rapidsms/backends/backend_http.py
@@ -3,6 +3,8 @@
 
 import BaseHTTPServer
 import thread
+import random
+import re
 from backend_base import Base
 
 
@@ -11,13 +13,43 @@ class Http(Base):
     class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         
         def do_GET(self):
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(self.path)
+            # if the path is just "/" then start a new session
+            # and redirect to that session's URL
+            if self.path == "/":
+                session_id = random.randint(100000, 999999)
+                self.send_response(301)
+                self.send_header("Location", "/%d/" % session_id)
+                self.end_headers()
+                return
+            
+            # if the path is of the form /integer/blah we want
+            # want to send a new message from integer with
+            # content blah
+            send_regex = re.compile(r"^/(\d{6})/(.*)")
+            match = send_regex.match(self.path)
+            if match:
+                # send the message
+                session_id = match.group(1)
+                text = match.group(2)
+                self.receive(session_id, text)
+                # respond with the number and text 
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write("Number: %s<br>Message: %s" % (session_id, text))
+                return
+                
             return
             
         def do_POST(self):
+            # TODO put this back in!
+            #send_regex = re.compile("^/(\d{6})/send")
+            #match = send_regex.match(self.path)
+            #if match:
+            #    # send the message to the router
+            #    session_id = match.group(1)
+            #    text = 
+            #    self.receive(session_id, text)
             return
     
     def __init__(self, router, host="localhost", port=8000):
