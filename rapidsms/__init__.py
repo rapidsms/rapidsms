d630db60dff6c0727d02abeef2ec7cd15af4a6d1
Mark E Johnston <mejymejy@gmail.com>
2009-02-25 12:51:15 -0500

    finished move to rapidsms in lib

--- rapidsms/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# vim: noet
-
-import app
-import router
-import spomsky
-from config import Config
-
-#__all__ = ("Config")
--- rapidsms/app.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# vim: noet
-
-class Base:
-	pass
