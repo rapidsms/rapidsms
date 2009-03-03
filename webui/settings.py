ce97507e22e49e60c4edc784f236de27926f3737
Mark E Johnston <mejymejy@gmail.com>
2009-03-03 18:31:10 -0500

    updated default sqlite3 db

--- webui/settings.py
+++ webui/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'rapidsms'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/tmp/rapidsms.sqlite3'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
