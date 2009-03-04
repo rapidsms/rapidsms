b651a0f7cb7a7a84d816617ab5b4dafc5ee01c45
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-04 17:18:43 -0500

    changed app.Base to app.App and updated the apps accordingly

--- lib/rapidsms/app.py
+++ lib/rapidsms/app.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-class Base:
-    pass
+class App(object):
+    
+    def __init__(self, router):
+        self.router = router
+        
+    def log(self, level, message):
+        router.log(level, message)
