c23cd25247974abc85c66451737f4de8d8b19d1b
Schuyler Erle <sderle@goldman.(none)>
2009-03-04 17:18:31 -0500

    Add a constructor method for Backend

--- lib/rapidsms/backends/backend.py
+++ lib/rapidsms/backends/backend.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-
 class Backend(object):
+    def __init__ (self, router):
+        self.router = router
     
     def log(self, level, message):
         self.router.log(level, message)
