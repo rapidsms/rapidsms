23f1ca58622611592d5ab8cf675fec3478becb8e
Schuyler Erle <sderle@goldman.(none)>
2009-03-04 17:44:33 -0500

    Add run() method to Backend and make start() a wrapper for it. Also set backend.running in backend.start and backend.stop. Whatever code runs in a loop in backend.run() needs to check self.running periodically to make sure it should still be running.

--- lib/rapidsms/backends/backend.py
+++ lib/rapidsms/backends/backend.py
@@ -4,15 +4,23 @@
 class Backend(object):
     def __init__ (self, router):
         self.router = router
+        self.running = False
     
     def log(self, level, message):
         self.router.log(level, message)
 
     def start(self):
+        self.running = True
+        try:
+            self.run()
+        finally:
+            self.running = False
+
+    def run (self):
         raise NotImplementedError
     
     def stop(self):
-        raise NotImplementedError
+        self.running = False
     
     def send(self):
         raise NotImplementedError
