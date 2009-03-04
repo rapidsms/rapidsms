ab534ccb62ecee27c6af5e110519545e78c721d9
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-04 17:43:47 -0500

    read config with eval instead of simplejson

--- lib/rapidsms/config.py
+++ lib/rapidsms/config.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import simplejson
 
 class Config():
 
     def __init__(self, path):
         # load the config, and parse it. i chose json because
         # it's in the python stdlib and is language-neutral
-        f = open(path)
-        self.raw = f.read()
-        self.data = simplejson.loads(self.raw)
+        self.data = eval(file(path).read())
 
     def __getitem__(self, key):
         return self.data[key]
