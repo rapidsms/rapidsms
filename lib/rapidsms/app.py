3d5534c426eccc6b1b073101318fa75890166d24
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-04 20:46:44 -0500

    added name property to apps

--- lib/rapidsms/app.py
+++ lib/rapidsms/app.py
@@ -7,6 +7,17 @@ class App(object, component.Component):
     
     def __init__(self, router):
         self.router = router
+            
+    def _get_name(self):
+        if hasattr(self, '_name'):
+            return self._name
+        else:
+            return unicode(type(self))   
+            
+    def _set_name(self, name):
+        self._name = name
+            
+    name = property(_get_name, _set_name)
 
     def start (self):
         pass
