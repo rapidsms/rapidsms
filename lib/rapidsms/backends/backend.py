76cd8eda16210f43cad48ac2d20ea395bad16999
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-04 20:48:24 -0500

    added name property to backends

--- lib/rapidsms/backends/backend.py
+++ lib/rapidsms/backends/backend.py
@@ -8,6 +8,17 @@ class Backend(object, component.Component):
     def __init__ (self, router):
         self.router = router
         self.running = False
+        
+    def _get_name(self):
+        if hasattr(self, '_name'):
+            return self._name
+        else:
+            return unicode(type(self))   
+
+    def _set_name(self, name):
+        self._name = name
+
+    name = property(_get_name, _set_name)
 
     def start(self):
         self.running = True
