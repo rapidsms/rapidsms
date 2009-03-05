9dfe9d6d6d5adc55204b8c87e531727d881f4635
Schuyler Erle <sderle@goldman.(none)>
2009-03-04 21:29:32 -0500

    Change the config module to use a .ini format, and move the module loading from server.py into config.py.

--- lib/rapidsms/config.py
+++ lib/rapidsms/config.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+import ConfigParser
 
-class Config():
 
-    def __init__(self, path):
-        # load the config, and parse it. i chose json because
-        # it's in the python stdlib and is language-neutral
-        self.data = eval(file(path).read())
+def to_list (item):
+    return map(lambda x: unicode(x).strip(), item.split(","))
+
+class Config (object):
+    def __init__(self, *paths):
+        self.parser = ConfigParser.ConfigParser()
+        self.data   = {}
+        self.parser.read(paths)
+        for section in map(unicode, self.parser.sections()):
+            data = {}
+            for option in self.parser.options(section):
+                data[option] = self.parser.get(section, option)
+            if hasattr(self, section):
+                data = getattr(self, section)(data)
+            self.data[section] = data
+
+    def load_apps (self, apps):
+        app_classes = []
+        for app_name in apps:
+            app_module_str = "apps.%s.app" % (app_name)
+            app_module = __import__(app_module_str, {}, {}, [''])
+            app_classes.append(app_module.App)
+        return app_classes
+
+    def load_backends (self, backends):
+        backend_classes = []
+        for backend_name in backends:
+            backend_module_str = "rapidsms.backends." + backend_name
+            backend_module = __import__(backend_module_str, {}, {}, [''])
+            backend_class = getattr(backend_module, backend_name.capitalize())
+            backend_classes.append(backend_class)
+        return backend_classes
+
+    def rapidsms (self, data):
+        app_classes = self.load_apps(to_list(data["apps"]))
+        backend_classes = self.load_backends(to_list(data["backends"]))
+        return { "apps": app_classes,
+                 "backends": backend_classes }
 
     def __getitem__(self, key):
         return self.data[key]
