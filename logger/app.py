5c036c5d87b1d9d2ddfce326fac4cdb16faa0a23
Schuyler Erle <sderle@goldman.(none)>
2009-03-05 13:29:23 -0500

    Pass args to superclass initializer in apps.Logger.App.

--- logger/app.py
+++ logger/app.py
@@ -7,10 +7,9 @@ import models
 from models import OutgoingMessage, IncomingMessage
 
 class App(rapidsms.app.App):
-    
-    def __init__(self):
-        super(rapidsms.app.App, self).__init__()
+    def __init__(self, *args):
         self.name = 'Logger'
+        super(rapidsms.app.App, self).__init__(*args)
 
     def incoming(self, message):
         msg = IncomingMessage(caller=message.caller, text=message.text, 
