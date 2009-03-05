0710fd8eb7772aa78dd52c96c66c2be02ab48bc2
Schuyler Erle <sderle@goldman.(none)>
2009-03-05 10:58:19 -0500

    Refactor Backend methods into component.Receiver, and inherit from it. Also, add a read-only router @property to Component, so that components don't just blow up when a log message is sent but no router is assigned.

--- lib/rapidsms/backends/backend.py
+++ lib/rapidsms/backends/backend.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import rapidsms.component
-import Queue
+from rapidsms.component import Component, Receiver
 
-class Backend(rapidsms.component.Component):
+class Backend (Component, Receiver):
     def __init__ (self, router):
         self.router = router
         self.running = False
-
-        # do we want to put a limit on the queue size?
-        # and what do we do if the queue gets full?
-        self._queue     = Queue.Queue()
-        
+       
     def _get_name(self):
         if hasattr(self, '_name'):
             return self._name
@@ -40,24 +35,11 @@ class Backend(rapidsms.component.Component):
     
     def stop(self):
         self._running = False
-    
-    def send(self, message):
-        # block until we can add to the queue.
-        # it shouldn't be that long.
-        self._queue.put(message, True)
-    
+   
     def receive(self, caller, text):
         # turn the caller/text into a message object
         msg = Message(self, caller, text)
         # and send it off to the router
         self.router.incoming(msg)
     
-    @property
-    def has_outgoing (self):
-        return self._queue.qsize()
- 
-    def outgoing (self):
-        try:
-            return self._queue.get_nowait()
-        except Queue.Empty:
-            return None
+
