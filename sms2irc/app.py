dae43bfcb246d140e058351d112cbb3c5717fefe
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-06 13:16:57 -0500

    sms2irc app

---
+++ sms2irc/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import rapidsms
+from rapidsms.message import Message
+
+# a pointless app to demonstrate the structure
+# of sms applications without magic decorators
+class App(rapidsms.app.App):
+
+    def start(self):
+        self.name = 'sms2irc'
+                  
+    def parse(self, message):
+        # TODO move backend lookup into start() once load order is finalized
+        self.backends = self.router.backends
+
+        for backend in self.backends:
+            print unicode(type(backend).__name__)
+            if unicode(type(backend).__name__) == unicode('Irc'):
+                self.irc_backend = backend
+            
+    def handle(self, message):
+        self.forward(message)
+
+    def outgoing(self, message):
+        #self.forward(message)
+        pass
+        
+    def forward(self, message):
+        self.info("%s: '%s' forwarded to %s" % (message.caller, message.text, self.irc_backend.channels))
+        txt = "(%s) says '%s'" % (message.backend.name, message.text)
+        Message(self.irc_backend, message.caller, txt).send()
