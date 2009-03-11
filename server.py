cb861319a6a5769b70673227a1c141ce86ee9068
Schuyler Erle <sderle@goldman.(none)>
2009-03-11 16:46:29 -0400

    Still debugging the handoff to the Django manager...

--- server.py
+++ server.py
@@ -3,5 +3,11 @@
 
 import sys
 import rapidsms
+import traceback
 
-rapidsms.manager.start_server(sys.argv)
+print "import server??"
+traceback.print_stack()
+
+if __name__ == "__main__":
+    
+    rapidsms.manager.start(sys.argv)
