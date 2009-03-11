1f7477f905fa114b8afad27560a0588d09c678b2
Schuyler Erle <sderle@goldman.(none)>
2009-03-11 16:46:29 -0400

    Still debugging the handoff to the Django manager...

--- lib/rapidsms/manager.py
+++ lib/rapidsms/manager.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import rapidsms
+from config import Config
+from router import Router
 
 def load_django_environment (conf):
     # import the webui (django) directory so that
     # we have the settings module for any apps
     # that may be using the database
     try:
-        import webui
-        import django
         from django.core.management import setup_environ
         from webui import settings
        
@@ -28,28 +27,36 @@ def load_django_environment (conf):
         # FIXME: TBD
 
         setup_environ(settings)
-        
+        return settings
+
     # TODO: proper logging here and everywhere!
     except ImportError, err:
         log.error("Couldn't import webui, check your webui settings module")
+        return None
 
-def django_manage_wrapper ():
-    pass
+def django_manage_wrapper (settings, argv):
+    from django.core.management import execute_manager
+    execute_manager(settings, argv) 
 
-def start_server (argv):
+def start (args):
     # load the local config file
-    if len(argv) > 1:
-        ini = argv[1]
+    if len(args) > 1:
+        ini = args.pop(1)
     else:
         ini = "rapidsms.ini"
-    conf = rapidsms.Config(ini)
+    conf = Config(ini)
 
+    django_settings = None
     if "database" in conf:
-        load_django_environment(conf)
-    
-    # load up the message router
-    router = rapidsms.router.Router(conf)
-    router.info("RapidSMS Server started up")
+        django_settings = load_django_environment(conf)
 
-    # wait for incoming messages
-    router.start()
+    print args
+    if len(args) > 1 and django_settings:
+        django_manage_wrapper(django_settings,args)
+    else:
+        # load up the message router
+        router = Router(conf)
+        router.info("RapidSMS Server started up")
+
+        # wait for incoming messages
+        router.start()
