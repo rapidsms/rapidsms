c904e26a2e4b197a64ad91ff42b8cd0704959504
unknown <dmyung@.(none)>
2009-03-11 19:01:41 -0400

    set environmental variables in to server to self configure with relative directory

--- server.py
+++ server.py
@@ -2,7 +2,14 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 import sys
+import os
+import traceback
+#os.environ["PYTHONPATH"] = os.path.join(os.getcwd(),'lib')
+sys.path.append(os.path.join(os.getcwd(),'lib'))
+    
 import rapidsms
+
+traceback.print_stack()
 
 if __name__ == "__main__":
     rapidsms.manager.start(sys.argv)
