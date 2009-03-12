ca94a95b852f975636253632fe181ff91e89cc0a
Schuyler Erle <sderle@goldman.(none)>
2009-03-12 19:18:49 -0400

    Add basic spomc backend test.

--- lib/rapidsms/tests/runtests.py
+++ lib/rapidsms/tests/runtests.py
@@ -7,6 +7,7 @@ from test_message import *
 from test_app import *
 from test_backend import *
 from test_backend_irc import *
+from test_backend_spomc import *
 
 if __name__ == "__main__":
     print "(some tests may pause for a few seconds, this is normal)"
