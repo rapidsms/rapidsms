4201325cca370358df14c0d3bafac58f30fe22d9
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-11 20:16:53 -0400

    removed stale __init__

--- logger/app.py
+++ logger/app.py
@@ -8,10 +8,6 @@ from models import OutgoingMessage, IncomingMessage
 
 class App(rapidsms.app.App):
     
-    def __init__(self, router):
-        super(rapidsms.app.App, self).__init__()
-        self.name = 'Logger'
-
     def incoming(self, message):
         msg = IncomingMessage(caller=message.caller, text=message.text, 
             received=message.received)
