f69fa20858c8ca3fe9cb0518487b09341cdf1d58
unknown <dmyung@.(none)>
2009-03-12 17:39:00 -0400

    initial inclusion of django environment bootstrapping of urls and template dirs.
    admin autodiscover() is having some issues with our integration project

--- webui/urls.py
+++ webui/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
