52870a22c2641ab5e437948e8f081e131ac60628
Mark E Johnston <mejymejy@gmail.com>
2009-03-12 19:03:28 -0400

    added tests for App class

--- lib/rapidsms/tests/runtests.py
+++ lib/rapidsms/tests/runtests.py
@@ -5,6 +5,7 @@ from test_config import*
 from test_log import *
 from test_backend import *
 from test_message import *
+from test_app import *
 
 if __name__ == "__main__":
     unittest.main()
---
+++ lib/rapidsms/tests/test_app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import unittest
+from rapidsms.app import App
+from harness import MockRouter
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.router = MockRouter()
+        self.name = "test"
+
+    def test___init__(self):
+        app = App(self.name, self.router)
+        self.assertEqual(self.router, app.router,
+            "failed to set router")
+        self.assertEqual(self.name, app.name,
+            "failed to set name")
+
+    def test_api(self):
+        app = App(self.name, self.router)
+        api_methods = (
+            'start',
+            'parse',
+            'handle',
+            'cleanup',
+            'outgoing',
+            'stop')
+        for method in api_methods:
+            self.assertTrue(hasattr(app, method), 
+                "app does not have attribute '%s'" % method)
+            self.assertTrue(callable(getattr(app, method)),
+                "attribute '%s' is not callable" % method)
+
+
+if __name__ == "__main__":
+    unittest.main()
