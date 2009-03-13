72f304be73fe8ba42aaf1cbefdfa2d671f9c7b26
Schuyler Erle <sderle@goldman.(none)>
2009-03-12 20:08:43 -0400

    Add MockApp and MockLogger to harness.py and add some router tests. They still need a lot of work, though.

--- lib/rapidsms/tests/runtests.py
+++ lib/rapidsms/tests/runtests.py
@@ -8,6 +8,7 @@ from test_app import *
 from test_backend import *
 from test_backend_irc import *
 from test_backend_spomc import *
+from test_router import *
 
 if __name__ == "__main__":
     print "(some tests may pause for a few seconds, this is normal)"
