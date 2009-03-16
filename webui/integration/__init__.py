e3c2a8178164c806351c03ec48b9e39dccd6362f
Evan Wheeler <evanmwheeler@gmail.com>
2009-03-16 13:50:15 -0400

    integrate urls and templatetags, patched poll to import properly

--- webui/integration/__init__.py
+++ webui/integration/__init__.py
@@ -2,6 +2,7 @@ print "do Django server integration!"
 from webui import settings
 from rapidsms.config import Config
 from django.conf.urls.defaults import *
+from django.templatetags import __path__
 import os
 import sys
 import string
@@ -21,15 +22,23 @@ for app_hash in conf["rapidsms"]["apps"]:
     app_name = app_hash['type']
     app_fullname = 'apps.' + app_name
     module = __import__(app_fullname, {}, {}, [''])
-    templates_to_add.append(os.path.join(os.path.dirname(module.__file__),'templates'))
+    templates_to_add.append(os.path.join(os.path.dirname(module.__file__),'webui/' + app_name + '/templates'))
     try:
-        urlmodule = module = __import__(app_fullname+".urls", {}, {}, [''])
-        urls_to_add += patterns('',(r'^%s/' % (app_name), include(app_fullname + ".urls")))
-    except:
-        continue
+        print app_fullname
+        #urlmodule = __import__(app_fullname+".webui.urls", {}, {}, [''])
+        urls_to_add += patterns('',(r'^%s/' % (app_name), include(app_fullname + ".webui.urls")))
+        # try to extend django.templatetags.__path__ with app's templatetags
+        __path__.extend(__import__(app_fullname + '.webui.' + app_name + '.templatetags', {}, {}, ['']).__path__)
+    except Exception, e:
+        print e
 
+print 'templates_to_add: ' + str(templates_to_add)
 settings.TEMPLATE_DIRS += templates_to_add
 
+print 'templatetags.__path__'
+print __path__
+
 from webui import urls
+print 'urls_to_add: ' + str(urls_to_add)
 urls.urlpatterns += urls_to_add
 
