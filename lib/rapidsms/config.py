fbc86f6b904ab789016a04c521576279501116cf
Adam Mckaig <adam.mckaig@gmail.com>
2009-03-19 18:10:23 -0400

    added basic webui for decision tree app

--- lib/rapidsms/config.py
+++ lib/rapidsms/config.py
@@ -1,61 +1,112 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import ConfigParser
+from ConfigParser import SafeConfigParser
 import log
 
 
 def to_list (item, separator=","):
-    return filter(None,map(lambda x: unicode(x).strip(), item.split(separator)))
+    return filter(None, map(lambda x: str(x).strip(), item.split(separator)))
+
 
 class Config (object):
-    def __init__(self, *paths):
-        self.parser = ConfigParser.ConfigParser()
-        self.data   = {}
+    def __init__ (self, *paths):
+        self.parser = SafeConfigParser()
         self.parser.read(paths)
-        sections = map(unicode, self.parser.sections())
         
-        # pass 1 - get the raw data
-        for section in sections:
-            data = {}
-            for option in self.parser.options(section):
-                data[option] = self.parser.get(section, option)
-            self.data[section] = data
+        self.raw_data = {}
+        self.data = {}
+        
+        # first pass: read in the raw data. it's all strings, since
+        # ConfigParser doesn't seem to decode unicode correctly (yet)
+        for sn in self.parser.sections():
+            items = self.parser.items(sn)
+            self.raw_data[sn] = dict(items)
 
-        # pass 2 - parse the specific sections
-        for section in sections:
-            # if this class has a parser specifically for this
-            # named section, call it and replace the raw contents
-            # (which are already in data[section]) with the result
-            section_parser = "parse_%s_section" % (section)
+        # second pass: iterate the raw data, creating a second
+        # dict (self.data) containing the "real" configuration,
+        # which may include things (magic, defaults, etc) not
+        # present in the raw_data
+        for sn in self.raw_data.keys():
+            section_parser = "parse_%s_section" % (sn)
+            
+            # if this section has a special parser, call
+            # it with the raw data, and store the result
             if hasattr(self, section_parser):
-                self.data[section] = \
-                    getattr(self, section_parser)(self.data, self.data[section])
+                self.data[sn] = \
+                    getattr(self, section_parser)(
+                        self.raw_data[sn])
+            
+            # no custom section parser, so
+            # just copy the raw data as-is
+            else:
+                self.data[sn] =\
+                    self.raw_data[sn]
 
-    def get_component_config(self, title, data):
-        component = data.get(title, {}).copy() # defaults to empty config
-        component["title"] = title
-        if not "type" in component:
-            component["type"] = title
-        return component
 
-    def parse_rapidsms_section (self, data, section):
-        app_titles     = to_list(section["apps"])
-        backend_titles = to_list(section["backends"])
-        apps     = [self.get_component_config(app_title, data) for app_title in app_titles]
-        backends = [self.get_component_config(backend_title, data) for backend_title in backend_titles]
-        return { "apps": apps,
-                 "backends": backends }
+    def component_section (self, name):
+        
+        # fetch the current config for this section
+        # from raw_data (or default to an empty dict),
+        # then copy it, so we don't alter the original
+        data = self.raw_data.get(name, {}).copy()
+        
+        # although "name" and "type" are deliberately distinct (to enable multiple
+        # components of the same type to run concurrently), it's cumbersome to have
+        # to provide a type every single time, so default to the name
+        if not "type" in data:
+            data["type"] = name
+        
+        try:
+            # attempt to import the "config" module from the app's package, to
+            # read in default configuration values. note that this DOES NOT
+            # import the app, just it's config.py
+            package_name = "apps.%s" % (data["type"])
+            module = __import__(package_name, {}, {}, ["config"])
+            if hasattr(module, "config"):
+                
+                # copy all of the names not starting with underscore (those are
+                # private or __magic__) into this component's default config
+                for var_name in dir(module.config):
+                    if not var_name.startswith("_"):
+                        data[var_name] = getattr(module.config, var_name)
+        
+        # no config module?
+        # doesn't really matter
+        except ImportError:
+            pass
+        
+        # apps should set a title in their "config" module,
+        # but in case they don't, recycle their name (again)
+        if not "title" in data:
+            data["title"] = name
+        
+        return data
 
-    def parse_log_section(self, data, section):
+
+    def parse_rapidsms_section (self, raw_section):
+        
+        # "apps" and "backends" are strings of comma-separated
+        # component names. first, break them into real lists
+        app_names     = to_list(raw_section["apps"])
+        backend_names = to_list(raw_section["backends"])
+        
+        # run lists of component names through component_section,
+        # to transform into lists of dicts containing more meta-info
+        return { "apps":     [self.component_section(n) for n in app_names],
+                 "backends": [self.component_section(n) for n in backend_names] }
+
+
+    def parse_log_section (self, raw_section):
         output = {"level": log.LOG_LEVEL, "file": log.LOG_FILE}
-        output.update(section)
+        output.update(raw_section)
         return output
 
-    def __getitem__(self, key):
+
+    def __getitem__ (self, key):
         return self.data[key]
         
-    def has_key(self, key):
+    def has_key (self, key):
         return self.data.has_key(key)
 
     __contains__ = has_key
