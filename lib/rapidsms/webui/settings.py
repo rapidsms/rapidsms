9c87bcd0073e72f7a4650c04fc1df24c3d46e9f3
adammck <adam.mckaig@gmail.com>
2009-03-26 13:39:33 -0400

    rejigged webui massively. DON'T PULL TO CORE. removed "integration" app, moved functionality into settings.py. on webui startup, urls.py from each rapidsms app is loaded and patched into the global urlpatterns, along with database settings, and any additional django settings (from the optional [django] section of the ini). these changes allow the sms backend and django frontend to share a single config, and remove the need to bundle a 'webui' directory with each deployment. DON'T PULL TO CORE until we've spoken with the group.

---
+++ lib/rapidsms/webui/settings.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import os, sys
+import urls
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/New_York'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '2rgmwtyq$thj49+-6u7x9t39r7jflu&1ljj3x2c0n0fl$)04_0'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = "rapidsms.webui.urls"
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    #"webui.integration.apps_context"
+]
+
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+]
+
+
+
+
+print "Loading RapidSMS WebUI..."
+
+
+
+
+# ====================
+# LOAD RAPIDSMS CONFIG
+# ====================
+
+# this module will usually be called up via the django
+# reloader, which is (in turn) called up by rapidsms's
+# server.py, which adds RAPIDSMS_INI to the environment.
+# just in case, though, check that it's defined rather
+# than repeating the filename selection logic here
+if not "RAPIDSMS_INI" in os.environ:
+    raise(
+        EnvironmentError,
+        "The RAPIDSMS_INI environment variable is not "  +\
+        "defined. Without it, settings.py doesn't know " +\
+        "which ini file to load settings from")
+
+# load the rapidsms configuration
+from rapidsms.config import Config
+conf = Config(os.environ["RAPIDSMS_INI"])
+
+# log to the screen. TODO: is there a proper
+# way of doing this in django? I can' find one
+print "Using config(s): %s" %\
+    (", ".join(conf.sources))
+
+
+
+
+# ====================
+# INJECT RAPIDSMS APPS
+# ====================
+
+rapidsms_apps = [
+    "apps.%s" % (rs_app["type"])
+    for rs_app in conf["rapidsms"]["apps"]
+]
+
+# TODO: move these?
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.admindocs'
+] + rapidsms_apps
+
+
+
+
+# ================
+# COLLECT URLS.PYs
+# ================
+
+imported = []
+failed = []
+
+for rs_app in conf["rapidsms"]["apps"]:
+    package_name = "apps.%s.urls" % (rs_app["type"])
+    
+    try:
+        module = __import__(package_name, {}, {}, ["urlpatterns"])
+        urls.urlpatterns += module.urlpatterns
+        imported.append(rs_app["title"])
+        
+    except ImportError:
+        failed.append(rs_app["title"])
+
+# log which apps were imported and failed
+if len(imported): print "Imported WebUI for: %s"         % (", ".join(imported))
+if len(failed):   print "Couldn't import WebUI for: %s" % (", ".join(failed))
+
+
+
+
+
+# ==========================
+# LOAD OTHER DJANGO SETTINGS
+# ==========================
+
+# load the database settings first, since those
+# keys don't correspond exactly to their eventual
+# name in this module (they're missing the prefix
+# in rapidsms.ini); inject them into this module
+if "database" in conf:
+    for key, val in conf["database"].items():
+        locals()["DATABASE_%s" % (key.upper())] = val
+
+else:
+    # database settings are missing, so
+    # blow up. TODO: is there a way to
+    # run django without a database?
+    raise(
+        ImproperlyConfigured,
+        "Your RapidSMS configuration ini (%r) does not " +\
+        "contain a [database] section, which is required " +\
+        "for the Django webui to function.")
+
+# if there is a "django" section, inject
+# the items as CONSTANTS in this module
+if "django" in conf:
+    for key, val in conf["django"].items():
+        locals()[key.upper()] = val
+
+
+
+
+# log separator
+# (end of rapidsms)
+print
