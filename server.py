506746aa5fde856fdd03889265b11af905bcd490
adammck <adam.mckaig@gmail.com>
2009-03-26 14:09:47 -0400

    removed traceback junk from server.py

--- server.py
+++ server.py
@@ -3,13 +3,10 @@
 
 import sys
 import os
-import traceback
 #os.environ["PYTHONPATH"] = os.path.join(os.getcwd(),'lib')
 sys.path.append(os.path.join(os.getcwd(),'lib'))
     
 import rapidsms
-
-traceback.print_stack()
 
 if __name__ == "__main__":
     rapidsms.manager.start(sys.argv)
