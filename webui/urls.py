8cce495248c6194d41c9588f1a57117b96178d94
adammck <adam.mckaig@gmail.com>
2009-03-29 12:48:57 -0400

    added "webui" app, to provide generic rapidsms "look" and global views

---
+++ webui/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import os
+from django.conf.urls.defaults import *
+import apps.webui.views as views
+
+urlpatterns = patterns('',
+    
+    url(r'^$', views.dashboard),
+    
+    (r'^static/(?P<path>.*)$', "django.views.static.serve",
+        {"document_root": os.path.dirname(__file__) + "/static"})
+)
+
