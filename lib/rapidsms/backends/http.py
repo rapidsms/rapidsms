8deee644b6d9ec5695572e2f2529d4fed3f5e934
Cory Zue <czue@dimagi.com>
2009-04-07 00:05:53 +0100

    adding Tim's changes for the http backend

--- lib/rapidsms/backends/http.py
+++ lib/rapidsms/backends/http.py
@@ -10,8 +10,11 @@ import urllib
 import rapidsms
 from rapidsms.message import Message
 
+msg_store = {}
+
 class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
+        global msg_store
         # if the path is just "/" then start a new session
         # and redirect to that session's URL
         if self.path == "/":
@@ -29,6 +32,16 @@ class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             # send the message
             session_id = match.group(1)
             text = match.group(2)
+            
+            if text == "json_resp":
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                
+                if msg_store.has_key(session_id) and len(msg_store[session_id]):
+                        self.wfile.write("{'%s'}" % str(msg_store[session_id].pop(0)))
+                return
+                
             # TODO watch out because urllib.unquote will blow up on unicode text 
             msg = self.server.backend.message(session_id, urllib.unquote(text))
             self.server.backend.route(msg)
@@ -62,8 +75,14 @@ class Backend(rapidsms.backends.Backend):
         self.server.backend = self
         
     def run (self):
+        global msg_store
         while self.running:
             if self.message_waiting:
                 msg = self.next_message()
                 # oops we have to throw the message away
+                if msg_store.has_key(msg.connection.identity):
+                        msg_store[msg.connection.identity].append(msg.text)
+                else:
+                        msg_store[msg.connection.identity] = []
+                        msg_store[msg.connection.identity].append(msg.text)
             self.server.handle_request()
