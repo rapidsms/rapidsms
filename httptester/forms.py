330e02bfef6b2549172e7898ff638ef698302fad
Cory Zue <czue@dimagi.com>
2009-04-07 03:26:28 +0100

    adding httpbasic stuff.  not sure if this is going through twice

--- httptester/forms.py
+++ httptester/forms.py
@@ -3,10 +3,10 @@ from django.forms import widgets
 from apps.httptester.models import Message
 
 class MessageForm(forms.ModelForm):
-    phone_number = forms.CharField(max_length=15, label=u'Phone Number')
+    #phone_number = forms.CharField(max_length=15, label=u'Phone Number')
     body = forms.Textarea()
     class Meta:
         model = Message
-        exclude = ("date", "outgoing")
+        exclude = ("date", "outgoing", "phone_number")
         
 
