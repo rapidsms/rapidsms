29b2b24198221d87045a05b11ea5e098c08baeb0
Schuyler Erle <sderle@goldman.(none)>
2009-04-08 17:06:15 -0400

    Bug fixes to the new management features.

--- lib/rapidsms/manager.py
+++ lib/rapidsms/manager.py
@@ -7,7 +7,7 @@ import os, sys, shutil
 
 # the Manager class is a bin for various RapidSMS specific management methods
 class Manager (object):
-    def route (conf, *args):
+    def route (self, conf, *args):
         router = Router()
         router.set_logger(conf["log"]["level"], conf["log"]["file"])
         router.info("RapidSMS Server started up")
@@ -23,17 +23,17 @@ class Manager (object):
         # wait for incoming messages
         router.start()
 
-    def _skeleton (tree):
+    def _skeleton (self, tree):
         return os.path.join(os.path.dirname(__file__), "skeleton", tree)
 
-    def startproject (conf, *args):
+    def startproject (self, conf, *args):
         name = args[0]
-        shutil.copytree(_skeleton("project"), name)
+        shutil.copytree(self._skeleton("project"), name)
 
-    def startapp (conf, *args):
+    def startapp (self, conf, *args):
         name = args[0]
         target = os.path.join("apps",name)
-        shutil.copytree(_skeleton("app"), target)
+        shutil.copytree(self._skeleton("app"), target)
         print "Don't forget to add '%s' to your rapidsms.ini apps." % name
 
 def start (args):
@@ -59,28 +59,32 @@ def start (args):
     # read the config, which is shared
     # between the back and frontend
     conf = Config(ini)
-    
-    # import the webui settings, which builds the django
-    # config from rapidsms.config, in a round-about way.
-    # can't do it until env[RAPIDSMS_INI] is defined
-    from rapidsms.webui import settings
 
-    # whatever we're doing, we'll need to call
-    # django's setup_environ, to configure the ORM
-    os.environ["DJANGO_SETTINGS_MODULE"] = "rapidsms.webui.settings"
-    from django.core.management import setup_environ, execute_manager
-    setup_environ(settings)
+    # if we found a config ini, try to configure Django
+    if conf.sources:
+        # import the webui settings, which builds the django
+        # config from rapidsms.config, in a round-about way.
+        # can't do it until env[RAPIDSMS_INI] is defined
+        from rapidsms.webui import settings
+
+        # whatever we're doing, we'll need to call
+        # django's setup_environ, to configure the ORM
+        os.environ["DJANGO_SETTINGS_MODULE"] = "rapidsms.webui.settings"
+        from django.core.management import setup_environ, execute_manager
+        setup_environ(settings)
+    else:
+        settings = None
 
     # if one or more arguments were passed, we're
     # starting up django -- copied from manage.py
-    if not args:
+    if len(args) < 2:
         print "Commands: route, startproject <name>, startapp <name>"
         sys.exit(1)
 
-    if args[0] in Manager:
-        handler = getattr(Manager, args[0])
-        handler(conf, *args[1:])
-    else:
+    if hasattr(Manager, args[1]):
+        handler = getattr(Manager(), args[1])
+        handler(conf, *args[2:])
+    elif settings:
         # none of the commands were recognized,
         # so hand off to Django
         execute_manager(settings)
