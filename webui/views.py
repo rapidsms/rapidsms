34fc32e4b87c4af9982fd8f0010d0cac0c384004
Schuyler Erle <sderle@goldman.(none)>
2009-04-11 15:13:29 -0400

    Remove an obviously mistaken reference to views in apps/tree.

--- webui/views.py
+++ webui/views.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from apps.tree.models import *
-
-
 def dashboard(req):
-	return render_to_response("dashboard.html", { },
-	    context_instance=RequestContext(req))
+    return render_to_response("dashboard.html", { },
+        context_instance=RequestContext(req))
