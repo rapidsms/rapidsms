03af22161bf8a6193d37ad39758e93ba524e44a5
Schuyler Erle <sderle@goldman.(none)>
2009-04-11 18:09:25 -0400

    Added new TestScript framework (including metatests!)

--- lib/rapidsms/tests/harness.py
+++ lib/rapidsms/tests/harness.py
@@ -1,4 +1,5 @@
 from rapidsms.router import Router
+from rapidsms.backends.backend import Backend
 from rapidsms.app import App
 
 # a really dumb Logger stand-in
@@ -16,6 +17,7 @@ class MockRouter (Router):
         self.backends.append(backend)
 
     def add_app (self, app):
+        app.configure()
         self.apps.append(app)
 
     def start (self):
@@ -27,6 +29,16 @@ class MockRouter (Router):
         self.running = False
         self.stop_all_backends()
 
+class MockBackend (Backend):
+    def start (self):
+        self._running = True
+        self.outgoing = []
+
+    def run (self):
+        while self.running:
+            msg = self.next_message(0.25)
+            if msg is not None: self.outgoing.append(msg) 
+ 
 # a subclass of App with all the moving parts replaced
 class MockApp (App):
     def configure (self):
@@ -49,3 +61,8 @@ class MockApp (App):
 
     def stop (self):
         self.calls.append(("stop",))
+
+class EchoApp (MockApp):
+    def handle (self, message):
+        MockApp.handle(self, message)
+        message.respond(message.peer + ": " + message.text)
