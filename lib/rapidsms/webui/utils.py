c7cbb5f243fdef99f240ab118e364af5b4e8b3a6
adammck <adam.mckaig@gmail.com>
2009-04-16 11:04:45 -0400

    added "conf" function to rapidsms.webui.utils
    this is super handy for altering behavior based on the combined rapidsms
    and django settings, without importing os.environ and Config all over.
    
    i also patched the query log to use the new method, as an example of its
    usage. to add more debug info to the webui, check out the templates and
    apps.webui.templatetags.debug.debug_dump() -- visibility can be toggled
    using the [django] debug=true setting

--- lib/rapidsms/webui/utils.py
+++ lib/rapidsms/webui/utils.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+
 import os
 from rapidsms.config import Config
 from django.template import loader
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response as django_r_to_r
 
+
 def render_to_response(template_name, dictionary=None, **kwargs):
     """Proxies calls to django.shortcuts.render_to_response, to avoid having
        to include the global variables in every request. This is a giant hack,
        and there's probably a much better solution."""
     
-    conf = Config(os.environ["RAPIDSMS_INI"])
-    rs_dict = {"apps": conf["rapidsms"]["apps"] }
+    rs_dict = {
+        "apps":  conf("rapidsms", "apps"),
+        "debug": conf("django", "debug")
+    }
     
     # allow the dict argument to
     # be omitted without blowing up
@@ -22,3 +26,25 @@ def render_to_response(template_name, dictionary=None, **kwargs):
     
     # pass on the combined dicts to the original function
     return django_r_to_r(template_name, rs_dict, **kwargs)
+
+
+def conf(section, key):
+    """Returns a value from the RapidSMS configuration file, as found in the
+       RAPIDSMS_INI environment variable. This introduces mild coupling between
+       the webui and backend, for the sake of convenience."""
+    
+    var = "RAPIDSMS_INI"
+    if not var in os.environ:
+        # "rapidsms.webui.utils.conf should only
+        # be called from within a running django
+        # server, where env[RAPIDSMS_INI] is defined"
+        raise KeyError(var)
+    
+    try:
+        return Config(os.environ[var])[section][key]
+    
+    # if the section or key (or both) were invalid,
+    # just return none. as far as we're concerned,
+    # absence is the same as False or None here
+    except KeyError:
+        return None
