0075b4ae98c30ac09320b25946af6275a1d87edf
Cory Zue <czue@dimagi.com>
2009-04-17 13:46:43 +0100

    cleaning up the forms logic, by making a base class in the forms app that others can extend.   moved the auto-creation of objects from forms to a utility method in that class.

--- nigeria/formslogic.py
+++ nigeria/formslogic.py
@@ -3,9 +3,9 @@
 
 from models import *
 from apps.reporters.models import *
+from apps.form.formslogic import FormsLogic
 
-
-class NigeriaFormsLogic:
+class NigeriaFormsLogic(FormsLogic):
     ''' This class will hold the nigeria-specific forms logic.
         I'm not sure whether this will be the right structure
         this was just for getting something hooked up '''
@@ -98,31 +98,6 @@ class NigeriaFormsLogic:
             # this borrows a lot from supply.  I think we can make this a utility call
             to_use = self._form_lookups[form_entry.form.type]
             form_class = to_use["class"]
-            instance = form_class()
-            for token_entry in form_entry.tokenentry_set.all():
-                if to_use.has_key(token_entry.token.abbreviation):
-                    field_name = to_use[token_entry.token.abbreviation]
-                    # this is sillyness.  this gets the model type from the metadata
-                    # and if it's a foreign key then "rel" will be non null
-                    foreign_key = instance._meta.get_field_by_name(field_name)[0].rel
-                    if foreign_key:
-                        # we can't just blindly set it, but we can get the class out 
-                        fk_class = foreign_key.to
-                        # and from there the name
-                        fk_name = fk_class._meta.object_name
-                        # and from there we look it up in our table
-                        field = self._foreign_key_lookups[fk_name]
-                        # get the object instance
-                        filters = { field : token_entry.data }
-                        try:
-                            fk_instance = fk_class.objects.get(**filters)
-                            setattr(instance, field_name, fk_instance)
-                        except fk_class.DoesNotExist:
-                            # ah well, we tried.  Is this a real error?  It might be, but if this
-                            # was a required field then the save() will fail
-                            pass
-                    else:
-                        setattr(instance, to_use[token_entry.token.abbreviation], token_entry.data)
-            instance.reporter = message.reporter
+            instance = self._model_from_form(message, form_entry, form_class, to_use, self._foreign_key_lookups)
             instance.save()
         
