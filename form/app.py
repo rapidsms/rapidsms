55b07c36a929cb014f552e5e88e0ebfc78f0d6fa
adammck <adam.mckaig@gmail.com>
2009-04-17 03:34:49 -0400

    added REGISTER to nigeria app, to register reporters

--- form/app.py
+++ form/app.py
@@ -188,7 +188,7 @@ class App(rapidsms.app.App):
     def form(self, app, message, code, type, *data): 
         self.debug("FORM")
         #reporter = self.__identify(message.peer, "reporting")
-        handled = False
+        self.handled = False
         for domain in self.domains_forms_tokens:
             if code.upper() in domain:
                 # gather list of form dicts for this domain code
@@ -207,6 +207,7 @@ class App(rapidsms.app.App):
 
                         self.debug("FORM MATCH")
                         info = []
+                        
                         for t, d in zip(tokens, data):
                             this_token = Token.objects.get(abbreviation=t[0])
                             token_entry = TokenEntry.objects.create(\
@@ -215,19 +216,47 @@ class App(rapidsms.app.App):
                             # abbreviations from the token tuple to the received data
                             info.append("%s=%s" % (t[0], d or "??"))
                         
-                        # validation block
+                        
+                        
+                        
+                        # call the validator methods, which return False on
+                        # success, or a list of error messages on failure
                         validation_errors = self._get_validation_errors(this_form, form_entry)
-                        # action block
+                        
+                        # if no errors were returned (from ANY
+                        # registered app), call the actions
                         if not validation_errors:
+                            before = len(message.responses)
                             self._do_actions(message, this_form, form_entry)
+                            after = len(message.responses)
+                            self.handled = True
                             
-                        # assemble and send response
-                        # TODO this should be handled by registered app
-                        message.respond("Received report for %s %s: %s.\nIf this is not correct, reply with CANCEL" % \
-                            (code.upper(), type.upper(), ", ".join(info)))
-                        # keep track of whether we have responded so we send only one 'Oops' message
-                        self.handled = True
+                            # if the action(s) didn't send any responses,
+                            # then send the default confirmation. this is
+                            # just for backwards compatibility, really...
+                            # actions SHOULD send their own confirmation
+                            if(after <= before):
+                                message.respond("Received report for %s %s: %s.\nIf this is not correct, reply with CANCEL" % \
+                                    (code.upper(), type.upper(), ", ".join(info)))                        
+                        # oh no! there were validation errors!
+                        # since we've already matched the domain
+                        # and form, we can be pretty sure that
+                        # this was a valid attempt at submitting
+                        # this form - so send back the errors,
+                        # and note that we've handled this one
+                        else:
+                            
+                            # TODO: chunk the errors into 140 chars
+                            message.respond("\n".join(validation_errors))
+                            self.handled = True
+                        
+                        
+                        # stop processing forms, move
+                        # on to the next domain (!?)
                         break
+
+
+
 
                     else:
                         continue        
@@ -260,6 +289,7 @@ class App(rapidsms.app.App):
                 app = self.form_handlers[app_name.name]
                 errors = getattr(app,'validate')(form_entry)
                 if errors: validation_errors.extend(errors)
+        print "VALIDATION ERRORS: %r" % validation_errors
         return validation_errors
         
     def _do_actions(self, message, form, form_entry):
--- nigeria/formslogic.py
+++ nigeria/formslogic.py
@@ -1,18 +1,73 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+
+from apps.reporters.models import *
+
+
 class NigeriaFormsLogic:
     ''' This class will hold the nigeria-specific forms logic.
         I'm not sure whether this will be the right structure
         this was just for getting something hooked up '''
-       
-    def validate(self, *args, **kwargs):
-        print "Nigeria validated!"
-        print "You passed in %s" % args[0]
-        
-        
-    def actions(self, *args, **kwargs):
-        print "Nigeria actions!"
-        print "You passed in %s" % args[0]
-        
     
+    
+    def validate(self, form_entry):
+        if form_entry.form.type == "register":
+            data = form_entry.to_dict()
+            print "\n\n%r\n\n" % data
+                
+            # check that ALL FIELDS were provided
+            required = ["location", "role", "password", "name"]
+            missing = [t for t in required if data[t] is None]
+            
+            # in case we need help, build a valid reminder string
+            help = ("%s register " % form_entry.domain.code.lower()) +\
+                " ".join(["<%s>" % t for t in required])
+                
+            # missing fields! collate them, and
+            # send back a friendly non-localized
+            # error message, then abort
+            if missing:
+                mis_str = ", ".join(missing)
+                return ["Missing fields: %s" % mis_str, help]
+            
+            try:
+                # attempt to load the location and role objects
+                # via their codes, which will raise if nothing
+                # was found TODO: combine the exceptions
+                data["location"] = Location.objects.get(code__iexact=data["location"])
+                data["role"]     = Role.objects.get(code__iexact=data["role"])
+            
+            except Location.DoesNotExist:
+                return ["Invalid location code: %s" %
+                    data["location"], help]
+            
+            except Role.DoesNotExist:
+                return ["Invalid role code: %s" %
+                    data["role"], help]
+            
+            # parse the name via Reporter
+            data["alias"], data["first_name"], data["last_name"] =\
+                Reporter.parse_name(data.pop("name"))
+            
+            # check that the name/alias
+            # hasn't already been registered
+            reps = Reporter.objects.filter(alias=data["alias"])
+            if len(reps):
+                return ["Already been registed: %s" %
+                    data["alias"], help]
+            
+            # all fields were present and correct, so copy them into the
+            # form_entry, for "actions" to pick up again without re-fetching
+            form_entry.rep_data = data
+            
+            # nothing went wrong. the data structure
+            # is ready to spawn a Reporter object
+            return None
+    
+    
+    def actions(self, message, form_entry):
+        if form_entry.form.type == "register":
+            rep = Reporter.objects.create(**form_entry.rep_data)
+            message.respond("Reporter %s (#%d) added" % (rep.alias, rep.pk))
+
