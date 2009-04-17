8a50a1df55248b2c497c29a6a2edbccfd81b7e1e
Solomon Adebayo <solomoniyi@gmail.com>
2009-04-17 02:02:56 +0100

    Built tree of locations for rendering in  template

---
+++ nigeria/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+title = "Reports"
+tab_link = "/reports"
+a = "Alpha"
+b = "Beta"
---
+++ nigeria/urls.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import os
+from django.conf.urls.defaults import *
+import apps.nigeria.views as views
+
+urlpatterns = patterns('',
+    url(r'^reports?$', views.index),
+    url(r'^reports/kano', views.kano),
+    url(r'^reports/test', views.tests),
+    url(r'^reports/test2', views.raw_tests),
+    url(r'^reports/treetest', views.raw_tests),
+    url(r'^reports/ajax', views.ajax),
+    (r'^static/nigeria/(?P<path>.*)$', "django.views.static.serve",
+        {"document_root": os.path.dirname(__file__) + "/static"}),
+)
