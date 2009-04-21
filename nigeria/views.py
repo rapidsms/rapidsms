284894c1527010c648810d843f9b161c6ca3dd01
Tim Akinbo <takinbo@gmail.com>
2009-04-21 16:14:34 +0100

    added logistics reports in the nigeria app

--- nigeria/views.py
+++ nigeria/views.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseServerError
 from django.template import RequestContext
 from apps.reporters.models import Location, LocationType
-from django.shortcuts import render_to_response
+from apps.supply.models import Shipment, Transaction
+from rapidsms.webui.utils import render_to_response
 from django.db import models
+from random import randrange, seed
 
 import sys
+
+locid = "20"
+lgas = Location.objects.filter(code=locid)[0].children.all()[0:10]
+stocklevel_perlocation_data = []
+seed()
+
+for lga in lgas:
+    stocklevel_perlocation_data.extend({'label': lga.name, 'data': [[len(stocklevel_perlocation_data)+1, randrange(1000, 5000, 100)]]})
 
 #Views for handling summary of Reports Displayed as Location Tree
 def index(req):
@@ -43,55 +53,63 @@ def index(req):
         states_dict[state.name]=lgas_dict
     
 
-    return render_to_response("nigeria/index.html",{}, context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/index.html",{})
 
+def logistics_summary(req, locid):
+    transactions = Transaction.objects.all().order_by("shipment__sent")[0:10].reverse()
+    return render_to_response(req, "nigeria/logistics_summary.html", {'transactions': transactions})
 
 def supply_summary(req, frm, to, range):
-    return render_to_response("nigeria/supply_summary.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/supply_summary.html")
 
 def bednets_summary(req, frm, to, range):
-    return render_to_response("nigeria/bednets_summary.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/bednets_summary.html")
 
 def coupons_summary(req, frm, to, range):
-    return render_to_response("nigeria/coupons_summary.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/coupons_summary.html")
 
 
 # Periodical Reporting  by day, week, month for coupons
 def coupons_daily(req, locid):
-    return render_to_response("nigeria/coupons_daily.html", context_instance=RequestContext(req))
+    location = False
+    try:
+        location_object = Location.objects.get(code=locid)
+        location = {'name': location_object.name}
+    except: pass
+    return render_to_response(req, "nigeria/coupons_daily.html", {'location': location})
 
 def coupons_weekly(req, locid):
-    return render_to_response("nigeria/coupons_weekly.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/coupons_weekly.html")
 
 def coupons_monthly(req, locid):
-    return render_to_response("nigeria/coupons_monthly.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/coupons_monthly.html")
 
 
 # Periodical Reporting  by day, week, month for bednets
 def bednets_daily(req, locid):
-    return render_to_response("nigeria/bednets_daily.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/bednets_daily.html")
 
 def bednets_weekly(req, locid):
-    return render_to_response("nigeria/bednets_weekly.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/bednets_weekly.html")
 
 def bednets_monthly(req, locid):
-    return render_to_response("nigeria/bednets_monthly.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/bednets_monthly.html")
 
 
 # Periodical Reporting  by day, week, month for supply
 #Builds daily supply reporting
 def supply_daily(req, locid):
-    return render_to_response("nigeria/supply_daily.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/supply_daily.html")
 
 #Builds weekly supply detail
 def supply_weekly(req, locid):
-    return render_to_response("nigeria/supply_weekly.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/supply_weekly.html")
 
 #Build Monthly supply detail
 def supply_monthly(req, locid):
-    return render_to_response("nigeria/supply_monthly.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/supply_monthly.html")
 
 #This portion of code is for testing
 def tests(req):
-    return render_to_response("nigeria/testpages/trees.html", context_instance=RequestContext(req))
+    return render_to_response(req, "nigeria/testpages/trees.html")
 
