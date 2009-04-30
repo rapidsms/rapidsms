09d41a2268308c13f59e7d0a11b7163760cce60b
Tim Akinbo <takinbo@gmail.com>
2009-04-30 07:10:34 +0100

    modified the AirtimePins model to recognize mobile networks and vend airtime pins when supplied with a valid phone number

--- nigeria/models.py
+++ nigeria/models.py
@@ -1,6 +1,6 @@
 # vim: ai sts=4 ts=4 et sw=4
+import re
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist
 from apps.reporters.models import Location, Reporter, PersistantConnection
 
 
@@ -78,15 +78,15 @@ def loc_nets_cards_post_init(sender, **kwargs):
 models.signals.post_init.connect(loc_nets_cards_post_init, sender=Location)
 
 class AirtimePins(models.Model):
+    '''Store all available Nigerian mobile networks. Still incomplete.'''
     NETWORK_TYPES = (
-        ('MTN', 'MTN Nigeria'),
-        ('GLO', 'Glo Mobile'),
-        ('ZAIN', 'Zain'),
-        ('VISAFONE', 'Visafone'),
-        ('ETISALAT', 'Etisalat'),
-        ('MULTILINKS', 'Multilinks'),
-        ('STARCOMMS', 'Starcomms'),
-        ('ZOOM', 'Zoom')
+        ('MTN', '^(\+?234|0)(703|706|803|806)\d{7}$'),
+        ('GLO', '^(\+?234|0)(705|805|807)\d{7}$'),
+        ('ZAIN', '^(\+?234|0)(708|802|808)\d{7}$'),
+        ('VISAFONE', '^(\+?234|0)(7040)\d{6}$'),
+        ('ETISALAT', '^(\+?234|0)(809)\d{7}$'),
+        ('MULTILINKS', '^(\+?234|0)(7027)\d{6}$'),
+        ('STARCOMMS', '^(\+?234|0)(181[26])\d{4}$'),
     )
 
     network = models.CharField(blank=True, null=True, max_length=20, choices=NETWORK_TYPES)
@@ -102,3 +102,11 @@ class AirtimePins(models.Model):
     def get_airtime(klass, network):
         '''Get an unused airtime pincode for a particular network'''
         return klass.objects.filter(used=False,network=network).all()[:1].get()
+
+    @classmethod
+    def vend_airtime(klass, phone_number):
+        '''Lookup the phone_number from available networks and vend a spare airtime pin for the number'''
+        for network in klass.NETWORK_TYPES:
+            if re.match(network[1], phone_number):
+                return klass.get_airtime(network[0])
+                break
