5706925bd4f5232d13802bd0bc72e5c8a1559254
adammck <adam.mckaig@gmail.com>
2009-05-04 21:54:07 -0400

    cleared up the distinction between Component.[name|type|title|caption]
    this is a disruptive change, which will probably break things. i've
    tried to find every spot that the distinction matters to our current
    deployment (nigeria), but i don't have time right now to fix the
    unrelated parts which will break. sorry. as usual...
    
    DO NOT PULL THIS TO CORE.
    IT WILL BREAK EVERTHING.
    I'M TALKING TO YOU, EVAN.
    YES, YOU.
    
    i'll try to explain why i feel this change is necessary.
    
    we often need to run multiple backends of the same type, with separate
    configurations. to do this, we made it possible to activate backends
    (and apps) either by their directory name, or arbitrarily-named [section]
    which contained a "type=whatever" option. this was cool, but as a result,
    it made enumerating backends or apps via rapidsms.config rather difficult,
    because you could never tell what the data structure would look like.
    
    to dodge this, i added a bunch of key/vals to the data structures in
    Config.component_section. unfortunately, i/we didn't clarify where each
    of them (type/title/name) should be used, so every time one of us needed
    to enumerate backends or apps, we accomplished it a freaky new way.
    
    NOW:
    
    components have a SLUG (filename+url safe name) and a TITLE (totally
    arbitrary text). to enumerate them, use router.backends and router.apps.
    if you need to enumerate them in the WebUI, ask the router to serialize
    them for you via HTTP (try the AJAX app, it's easy), and enumerate that.
    
    this is another of those things that should have been discussed with the
    group, but it's 3:15, and i'm in nigeria. so... it'll have to wait.

--- reporters/models.py
+++ reporters/models.py
@@ -251,28 +251,6 @@ class Reporter(models.Model):
         return max(timedates) if timedates else None
 
 
-class BackendManager(models.Manager):
-    def get_query_set(self):
-        
-        # fetch a list of all the backends
-        # that we already have objects for
-        known_backends  = PersistantBackend.raw_objects.values_list("title", flat=True)
-        
-        # and a list of the backends which are
-        # running, which we SHOULD have objects for 
-        running_backends = [be["title"] for be in conf["rapidsms"]["backends"]]
-        
-        # find any running backends which currently
-        # don't have objects, and fill in the gaps
-        for t in running_backends:
-            if not t in known_backends:
-                PersistantBackend(title=t).save()
-        
-        # now that we're sure the backends table is up
-        # to date, continue fetching the queryset as usual
-        return super(BackendManager, self).get_query_set()
-
-
 class PersistantBackend(models.Model):
     """This class exists to provide a primary key for each
        named RapidSMS backend, which can be linked from the
@@ -280,14 +258,9 @@ class PersistantBackend(models.Model):
        (in models which wish to link to a backend), since the
        available backends (and their orders) may change after
        deployment; hence, something persistant is needed."""
-    title       = models.CharField(max_length=30, unique=True)
-    raw_objects = models.Manager()
-    objects     = BackendManager()
+    slug        = models.CharField(max_length=30, unique=True)
+    title       = models.CharField(max_length=30)
     
-    @property
-    def name(self):
-        # this is to be consistent with backend object
-        return self.title
     
     class Meta:
         verbose_name = "Backend"
@@ -303,9 +276,8 @@ class PersistantBackend(models.Model):
             object. In time, this should be moved to the message object itself, since persistance
             should be fairly ubiquitous; but right now, that would couple the framework to this
             individual app. So you can use this for now."""
-        be_title = msg.connection.backend.name
-        return klass.objects.get(title=be_title)
-
+        be_slug = msg.connection.backend.slug
+        return klass.objects.get(slug=be_slug)
 
 
 class PersistantConnection(models.Model):
@@ -359,6 +331,7 @@ class PersistantConnection(models.Model):
             it as the implicit default connection for the Reporter. """
         self.last_seen = datetime.now()
         return self.save()
+    
     
     def prefer(self):
         """Removes the _preferred_ flag from all other PersistantConnection objects
