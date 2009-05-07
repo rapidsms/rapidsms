ad268aea80569bdf264b65a531e732a33d4083c4
adammck <adam.mckaig@gmail.com>
2009-05-07 13:49:35 -0400

    added auto-populated "javascripts" array to the default context.
    since the django templating language (which is a "language" in the same
    way that wax crayons are an "art supply") provides no way to _add_ to
    a block without replacing the original, dynamically generating a list
    of <script> tags is a chore.
    
    i am attempting to solve this with (surprise!) magic, by including
    providing a standard place for apps to put scripts which they want to
    be included on every page, and another to put scripts which they wish to
    include throughout the app. this will free up the {% javascripts %}
    block for individual templates to use for their own private scripts.

--- lib/rapidsms/webui/utils.py
+++ lib/rapidsms/webui/utils.py
@@ -2,7 +2,8 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-from rapidsms.config import conf
+import os, re, traceback
+from rapidsms.config import conf, app_conf
 from django.template import loader
 from django.template import RequestContext
 from django.http import HttpResponse, Http404
@@ -16,8 +17,44 @@ def render_to_response(req, template_name, dictionary=None, **kwargs):
     
     rs_dict = {
         "apps":  conf("rapidsms", "apps"),
-        "debug": conf("django", "debug")
+        "debug": conf("django", "debug"),
+        "javascripts": []
     }
+    
+    def __js_dir(fs_path, web_prefix):
+        """Adds all of the .js files in a given directory to the
+           javascripts array, to be included in the <head>. If the
+           directory does not exist, this function does nothing."""
+        if os.path.exists(fs_path):
+            rs_dict["javascripts"].extend([
+                "%s/%s" % (web_prefix, fn)
+                for fn in os.listdir(fs_path)
+                if fn[-3:] == ".js"])
+    
+    # add all of the global javascript files for all running
+    # apps. this is super handy for packaging functionality
+    # which affects the whole webui without hard-coding it
+    for app in conf("rapidsms", "apps"):
+        __js_dir(
+            "%s/static/javascripts/global" % app["path"],
+            "/static/%s/javascripts/global" % app["type"])
+    
+    # A NEW KIND OF LUNACY: inspect the stack to find out
+    # which rapidsms app this function is being called from
+    tb = traceback.extract_stack(limit=2)
+    m = re.match(r'^.+/(.+?)/views\.py$', tb[-2][0])
+    if m is not None:
+        
+        # since we're fetching the app conf, add it to the
+        # template dict. it wouldn't be a very good idea to
+        # use it, but sometimes, when time is short...
+        rs_dict["app_conf"] = app_conf(m.group(1))
+        
+        # find all of the javascript assets for
+        # this app, and add them to the <head>
+        __js_dir(
+            "%s/static/javascripts/app" % rs_dict["app_conf"]["path"],
+            "/static/%s/javascripts/app" % rs_dict["app_conf"]["type"])\
     
     # allow the dict argument to
     # be omitted without blowing up
