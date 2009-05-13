05dceaf4a2eddc65dff5347e4cc2db9b325299b1
adammck <adam.mckaig@gmail.com>
2009-05-13 07:38:01 -0400

    moved pagination helper into core utils.
    updated reporters and messaging app to use it. as a result, the
    messaging app is broken (can't select recipients). not ready for
    merging into master yet.

--- messaging/views.py
+++ messaging/views.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-from django.core.paginator import Paginator
-from rapidsms.webui.utils import render_to_response
+from rapidsms.webui.utils import *
 from apps.reporters.models import *
 
 
 def index(req):
-    all_reporters = Reporter.objects.all()
-    paginator = Paginator(all_reporters, 20)
-
-    try:
-        page = int(req.GET.get("p", "1"))
-        
-    except ValueError:
-        page = 1
-    
-    
-    try:
-        reporters = paginator.page(page)
-        
-    except (EmptyPage, InvalidPage):
-        reporters = paginator.page(paginator.num_pages)
-    
-    
     return render_to_response(req,
         "messaging/index.html", {
-        "reporters": reporters
-    })
+            "columns": ["Name", "Role", "Location"],
+            "reporters": paginated(req, Reporter.objects.all()) })
