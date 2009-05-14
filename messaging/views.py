92c2d39b2d374c8b1a314cf840f845e1af44d085
adammck <adam.mckaig@gmail.com>
2009-05-13 23:07:08 -0400

    added optional "wrapper" argument to utils.paginated
    before returning, the object_list (the items that will be displayed on
    the visible page) are all mapped by the wrapper function. this is super
    useful for customizing the output, without patching every single object
    in the input (which would force them all to be fetched from the db)

--- messaging/views.py
+++ messaging/views.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+import re, urllib
 from rapidsms.webui.utils import *
 from apps.reporters.models import *
 
 
 def index(req):
+    flat_checked = urllib.unquote(req.COOKIES.get("recipients", ""))
+    checked = re.split(r'\s+', flat_checked)
+    
+    def __reporter(rep):
+        rep.checked = str(rep.pk) in checked
+        return rep
+    
     return render_to_response(req,
         "messaging/index.html", {
             "columns": ["Name", "Role", "Location"],
-            "reporters": paginated(req, Reporter.objects.all()) })
+            "reporters": paginated(req, Reporter.objects.all(), wrapper=__reporter) })
