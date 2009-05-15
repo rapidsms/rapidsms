8ee83651f0d30f215d23ce5f1619a1f7a4e036d9
adammck <adam.mckaig@gmail.com>
2009-05-14 22:58:55 -0400

    revamped the messaging UI
    selected recipients are now persistant (which is essential when there
    are more reporters than could fit on a single page), and the interface
    does everything that i can think of to make blasting messages... fun!

--- messaging/app.py
+++ messaging/app.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+
 import rapidsms
+from apps.reporters.models import *
+
 
 class App (rapidsms.app.App):
-    def ajax_GET_recipients(self, params):
-        return [
-            {"app": app, "recipients": app.ajax_GET_recipients(params)}
-            for app in self.router.apps
-            if (app != self) and hasattr(app, "ajax_GET_recipients")]
+    def ajax_POST_send_message(self, params, form):
+        rep = Reporter.objects.get(pk=form["uid"])
+        pconn = rep.connection()
+        
+        # abort if we don't know where to send the message to
+        # (if the device the reporter registed with has been
+        # taken by someone else, or was created in the WebUI)
+        if pconn is None:
+            raise Exception("%s is unreachable (no connection)" % rep)
+        
+        # attempt to send the message
+        # TODO: what could go wrong here?
+        be = self.router.get_backend(pconn.backend.slug)
+        return be.message(pconn.identity, form["text"]).send()
--- messaging/views.py
+++ messaging/views.py
@@ -7,14 +7,23 @@ from apps.reporters.models import *
 
 
 def index(req):
-    flat_checked = urllib.unquote(req.COOKIES.get("recipients", ""))
-    checked = re.split(r'\s+', flat_checked)
+    def cookie_recips(status):
+        flat = urllib.unquote(req.COOKIES.get("recip-%s" % status, ""))
+        return re.split(r'\s+', flat) if flat != "" else []
+    
+    checked = cookie_recips("checked")
+    error   = cookie_recips("error")
+    sent    = cookie_recips("sent")
     
     def __reporter(rep):
-        rep.checked = str(rep.pk) in checked
+        uid = str(rep.pk)
+        rep.is_checked = uid in checked
+        rep.is_error   = uid in error
+        rep.is_sent    = uid in sent
         return rep
     
     return render_to_response(req,
         "messaging/index.html", {
             "columns": ["Name", "Role", "Location"],
+            "num_checked": len(checked),
             "reporters": paginated(req, Reporter.objects.all(), wrapper=__reporter) })
