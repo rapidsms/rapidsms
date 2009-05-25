7097a0590a04483226b80b434063e48cb3389536
adammck <adam.mckaig@gmail.com>
2009-05-25 11:11:36 +0300

    reinstated {% region %} and {% debug_dump %} tags in their own modules
    this has actually been broken for a few days, since i accidentally the
    whole {% webui %} tag without removing it from layout.html -- sorry,
    to anyone that's trying to apply these commits incrementally.

---
+++ webui/templatetags/debug_dump.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from django.db import connection
+
+from django import template
+register = template.Library()
+
+
+@register.inclusion_tag("webui/partials/debug-dump.html")
+def debug_dump():
+    return { "queries": connection.queries }
