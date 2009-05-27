e805de4b05097a5a3f58d9d2eaa87efe10e1ce93
adammck <adam.mckaig@gmail.com>
2009-05-27 04:08:30 +0300

    moved RecursiveManager into its own module
    i'm about to start splitting the Location stuff out of the reporters
    app, and both apps have recursive models (Group and Location), so i'm
    extracing this in advance.

--- reporters/models.py
+++ reporters/models.py
@@ -6,6 +6,7 @@ import os, re
 from datetime import datetime
 from django.db import models
 
+from rapidsms.webui.managers import RecursiveManager
 from apps.patterns.models import Pattern
 
 
@@ -36,28 +37,6 @@ class LocationType(models.Model):
         
     def __unicode__(self):
         return self.name
-    
-
-class RecursiveManager(models.Manager):
-    """Provides a method to flatten a recursive model (a model which has a ForeignKey field linked back
-       to itself), in addition to the usual models.Manager methods. This Manager queries the database
-       only once (unlike select_related), and sorts them in-memory. Obivously, this efficiency comes
-       at the cost local inefficiency -- O(n^2) -- but that's still preferable to recursively querying
-       the database."""
-    
-    def flatten(self, via_field="parent_id"):
-        all_objects = list(self.all())
-        
-        def pluck(pk=None, depth=0):
-            output = []
-            
-            for object in all_objects:
-                if getattr(object, via_field) == pk:
-                    output += [object] + pluck(object.pk, depth+1)
-                    object.depth = depth
-            
-            return output
-        return pluck()
 
 
 class Location(models.Model):
