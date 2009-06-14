29f364563b0dc6e7fa648f67327189dae75f3fb4
Jeff Wishnie <jeff@wishnie.org>
2009-06-14 21:43:11 +0000

    debian/ubuntu start-up scripts

---
+++ dist/debian_ubuntu/rapidsms-init.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+#
+# IMPORTANT: To use, do the folling:
+#
+# 1. Change 'NAME' variable to the name of your project. E.g. "bednets_for_nigeria"
+# 2. Place this file in the TOP-LEVEL of your project, right where 'manage.py' is
+# 3. Link it into /etc/init.d e.g. > ln -s /usr/local/my_project/rapidsms-init.sh /etc/init.d/
+# 4. Add it to the runlevels, on Ubuntu/Debian there is a nice tool to do this for you:
+#    > sudo update-rc.d rapidsms-init.sh defaults
+#
+# NOTE: If you want to run multiple instances of RapidSMS, just put this init file in each project dir,
+#       set a different NAME for each project, link it into /etc/init.d with _different_ names,
+#       and add _each_ script to the runlevels.
+#
+
+### BEGIN INIT INFO
+# Provides:          amatd daemon instance
+# Required-Start:    $all
+# Required-Stop:     $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts instance of amatd daemon
+# Description:       starts instance of amatd daemon using start-stop-daemon
+### END INIT INFO
+
+# set -e
+
+ME=`readlink -f $0`
+WHERE_AM_I=`dirname $ME`
+
+############### EDIT ME ##################
+NAME="smsforum" # change to your project name
+DAEMON=$WHERE_AM_I/manage.py
+DAEMON_OPTS=""
+RUN_AS=root
+APP_PATH=$WHERE_AM_I
+ROUTER_PID_FILE=/var/run/${NAME}_router.pid
+WEBSERVER_PID_FILE=/var/run/${NAME}_webs.pid
+
+############### END EDIT ME ##################
+test -x $DAEMON || exit 0
+
+do_start() {
+    echo -n "Starting router for $NAME... "
+    start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $ROUTER_PID_FILE  --make-pidfile --exec $DAEMON -- route $DAEMON_OPTS
+    echo "Router Started"
+    sleep 2
+    echo -n "Starting webserver for $NAME... "
+    start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $WEBSERVER_PID_FILE  --make-pidfile --exec $DAEMON -- runserver $DAEMON_OPTS
+    echo "Webserver Started"
+}
+
+do_stop() {
+    echo -n "Stopping webserver for $NAME... "
+    start-stop-daemon --stop --pidfile $WEBSERVER_PID_FILE
+    echo "Webserver Stopped"
+    sleep 2
+    echo -n "Stopping webserver for $NAME... "
+    start-stop-daemon --stop --pidfile $ROUTER_PID_FILE
+    echo "Router Stopped"
+}
+
+case "$1" in
+  start)
+        do_start
+        ;;
+
+  stop)
+        do_stop
+        ;;
+
+  restart|force-reload)
+        do_stop
+        sleep 2
+        do_start
+        ;;
+
+  *)
+        echo "Usage: $ME {start|stop|restart|force-reload}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
