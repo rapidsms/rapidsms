bed2b34fa9995001d4a4512721240ed3c2e915fa
Rowena <rowena@rowena-vm.(none)>
2009-06-28 12:48:18 +0000

    clarify file names

--- scripts/check_tostan_translations.sh
+++ scripts/check_tostan_translations.sh
@@ -1,4 +1,4 @@
 for i in 'deb' 'dyu' 'en' 'fr' 'pul' 'wol'
 do
-     python check_translation.py ../apps/smsforum/locale/$i/LC_MESSAGES/django.po
+     python is_gsm_compatible.py ../apps/smsforum/locale/$i/LC_MESSAGES/django.po
 done
