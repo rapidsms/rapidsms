78692d639f38f0dedcd7dd1829a9625bb033844d
adammck <adam.mckaig@gmail.com>
2009-06-30 17:03:04 -0400

    added "backends" app, to display signal strength in the header
    currently, only the pyGSM backend actually provides a signal strength,
    and the spomc backend returns "unknown". no other backends show up. to
    provide status information from a backend, just add a "status" method,
    which returns a hash, like so (from gsm.py (almost)):
    
    class Backend(backends.Backend):
        _title = "pyGSM"
    
        # ...
    
        def status(self):
            return {
    	    "_title": self.title,
                "Sent": self.sent_messages,
                "Received": self.received_messages,
                "_signal": self.modem.signal_strength()
            }
    
    keys starting with _underscore are assumed to be internal, so aren't
    displayed. the "_title" key is (obviously) the title of the backend,
    and the "_signal" key determines which rapidsms logo is displayed.
    this isn't terribly well documented yet, but is fun and awesome.

--- lib/rapidsms/backends/gsm.py
+++ lib/rapidsms/backends/gsm.py
@@ -26,6 +26,8 @@ class Backend(BackendBase):
 
 
     def send(self, message):
+        self.sent_messages += 1
+        
         self.modem.send_sms(
             str(message.connection.identity),
             message.text)
@@ -33,20 +35,41 @@ class Backend(BackendBase):
 
     def gsm_log(self, modem, str, level):
         self.debug("%s: %s" % (level, str))
-    
-    
+
+
+    def status(self):
+        csq = self.modem.signal_strength()
+
+        # convert the "real" signal
+        # strength into a 0-4 scale
+        if   csq == 99: level = 0
+        elif csq >= 35: level = 4
+        elif csq >= 25: level = 3
+        elif csq >= 15: level = 2
+        else:           level = 1
+
+        return {
+            "_signal": level,
+            "_title": self.title,
+            "Sent": self.sent_messages,
+            "Received": self.received_messages
+        }
+
+
     def run(self):
         while self.running:
             self.info("Polling modem for messages")
             msg = self.modem.next_message()
-        
+
             if msg is not None:
+                self.received_messages += 1
+                
                 # we got an sms! create RapidSMS Connection and
                 # Message objects, and hand it off to the router
                 c = Connection(self, msg.sender)
                 m = Message(c, msg.text)
                 self.router.send(m)
-            
+
             # wait for POLL_INTERVAL seconds before continuing
             # (in a slightly bizarre way, to ensure that we abort
             # as soon as possible when the backend is asked to stop)
@@ -56,11 +79,14 @@ class Backend(BackendBase):
 
 
     def start(self):
+        self.sent_messages = 0
+        self.received_messages = 0
+        
         self.modem = pygsm.GsmModem(
             logger=self.gsm_log,
             *self.modem_args,
             **self.modem_kwargs)
-        
+
         # If we got the connection, call superclass to
         # start the run loop--it just sets self._running to True
         # and calls run.
--- lib/rapidsms/backends/spomc.py
+++ lib/rapidsms/backends/spomc.py
@@ -1,47 +1,61 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+
+import re, time
 from rapidsms.message import Message
 from rapidsms.connection import Connection
 import backend
 import spomsky
-import re, time
+
 
 class Backend(backend.Backend):
     _title = "SPOMC"
-    
+
+
     def configure(self, host="localhost", port=8100, **kwargs):
         self.client = spomsky.Client(host, port)
-        
+
+
+    def status(self):
+        return {
+            "_signal": None,
+            "_title": self.title
+        }
+
+
     def __callback(self, source, message_text):
         # drop the "sms://" protocol from the source
         phone_number = re.compile("[a-z]+://").sub("", source)
-        
+
         # create connection and message objects, and
         # pass it off to the router
         c = Connection(self, phone_number)
         m = Message(c, message_text)
         self.router.send(m)
 
+
     def send(self, message):
         destination = "%s" % (message.connection.identity)
         sent = self.client.send(destination, message.text)
-        
+
         # if the message wasn't sent, log it to the super
         # low-tech pending file, to be sent at some point
         # in the undefined future...
         if not sent:
-        
+
             # TODO: move this to backend.py, and make it sane
             str = "%s:%s:%s" % (self.slug, destination, message.text)
             self.warning("DIDN'T SEND: %s" % (str))
             f = file("/tmp/rapidsms-pending", "a")
             f.write(str + "\n")
             f.close()
-    
+
+
     def start(self):
         self.client.subscribe(self.__callback)
         backend.Backend.start(self)
+
 
     def stop(self):
         backend.Backend.stop(self)
