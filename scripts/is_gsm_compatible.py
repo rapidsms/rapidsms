514aa3e563ad2b46958dd76cd864a28fb7e9b4e3
U-rotop\Rowena <Rowena@rotop.(none)>
2009-07-02 17:22:01 -0400

    improved gsm validator to attempt reading file in n different encodings

--- scripts/is_gsm_compatible.py
+++ scripts/is_gsm_compatible.py
@@ -1,37 +1,62 @@
 """ Translation Checker
 
-Run this function with various tranlsation files (typically *.po from gettext)
+Run this function with various utf-8 tranlsation files (typically *.po from gettext)
 and it will check whether they can be gsm-encoded
 """
 from __future__ import with_statement
 import sys, os
 import codecs
+import getopt
 
 # hack to get paths working - we should use django's runscript instead
 CUR_DIR = os.getcwd()
 os.chdir('..')
 sys.path.append(os.path.join(os.getcwd(),os.path.join('lib','pygsm')))
 
-import getopt
 import gsmcodecs
 
-def check_gsm(arg):
+# attempt to interpret the input file in this order
+encodings = ['utf-8','cp1252','utf-16']
+line_count = 0
+
+def check_gsm_compatible(file):
+    file_name = file.rsplit(os.sep,1)[1]
+    for enc in encodings:
+        try: 
+            if can_gsm_encode(enc, file):
+                print "PASSES gsm encoding (%s)" % file_name
+                break
+        except UnicodeEncodeError, e:
+            print "FAILS GSM ENCODING (%s)! (file was read as type %s)" % (file_name,enc)
+            print "  Failed on line %d" % line_count
+            print "  " + str(e)
+            break
+    return
+
+def can_gsm_encode(encoding,file_name): 
+    """ Attempts the gsm conversion and return true if everything is good
+        Returns false if 'encoding' was the wrong encoding for the given file
+        Raises UnicodeEncodeError if there is a non-gsm character in the given file
+        Enc: the encoding of the input file
+        File_name: the file to process
+    """
+    global line_count
+    line_count = 0
     try:
-        line_count = 0;
-        file_name = os.path.join(CUR_DIR,arg)
-        with codecs.open(file_name,'r', encoding='utf-8') as fin:
+        # first assume we are getting utf-8 encoding (linux default)
+        with codecs.open(file_name,'r', encoding=encoding) as fin:
           for line in fin:
             line_count = line_count+1
             line.encode('gsm')
-    except UnicodeEncodeError as e:
-        print "FAILS GSM ENCODING (%s)!" % arg
-        print "Failed on line %d" % line_count
-        print e
-        return
-    print "PASSES gsm encoding (%s)" % arg
+        return True
+    except UnicodeDecodeError, e:
+        """ This happen when the input file is not of type 'encoding' """
+        print "  Cannot parse file as " + encoding
+        return False
 
+
+# generic wrapper for python scripts
 def main():
-    # parse command line options
     try:
         opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
     except getopt.error, msg:
@@ -47,7 +72,8 @@ def main():
     if len(args) < 1:
         print "ERROR: I need at least 1 argument"
     for arg in args:
-        check_gsm(arg) # process() is defined elsewhere
+        file = os.path.join(CUR_DIR,arg)
+        check_gsm_compatible(file) # process() is defined elsewhere
 
 if __name__ == "__main__":
     main()
