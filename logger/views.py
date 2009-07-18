138f8368fcae293de3af62fabddd69e74037c340
U-rotop\rowena <rowena@rotop.(none)>
2009-07-17 22:42:10 -0400

    Paginate message log

--- logger/views.py
+++ logger/views.py
@@ -2,6 +2,7 @@ from rapidsms.webui.utils import render_to_response
 from models import *
 from datetime import datetime, timedelta
 from utilities.export import export
+from rapidsms.webui.utils import paginated
 
 def index(req):
     template_name="logger/index.html"
@@ -12,7 +13,9 @@ def index(req):
     [ all.append(msg) for msg in outgoing]
     # sort by date, descending
     all.sort(lambda x, y: cmp(y.date, x.date))
-    return render_to_response(req, template_name, { "messages": all})
+    context = {}
+    context['messages'] = paginated(req, all, per_page=50)
+    return render_to_response(req, template_name, context )
 
 def csv_in(req, format='csv'):
     context = {}
