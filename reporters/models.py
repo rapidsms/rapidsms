a11cd8b6caa8db355a709bad0b6627cf10535339
adammck <adam.mckaig@gmail.com>
2009-07-27 12:06:19 -0400

    big whitespace cleanup in reporters app

--- reporters/models.py
+++ reporters/models.py
@@ -14,18 +14,18 @@ class ReporterGroup(models.Model):
     parent      = models.ForeignKey("self", related_name="children", null=True, blank=True)
     description = models.TextField(blank=True)
     objects     = RecursiveManager()
-    
-    
+
+
     class Meta:
         verbose_name = "Group"
 
     
     def __unicode__(self):
         return self.title
-    
-    
+
+
     # TODO: rename to something that indicates
-    #       that it's a counter, not a queryset    
+    #       that it's a counter, not a queryset
     def members(self):
         return self.reporters.all().count()
 
@@ -60,30 +60,30 @@ class Reporter(models.Model):
     #   klingon  = tlh
     #
     language = models.CharField(max_length=10, blank=True)
-	
+
     # although it's impossible to enforce, if a user registers
     # themself (via the app.py backend), this flag should be set
     # indicate that they probably shouldn't be trusted
     registered_self = models.BooleanField()
-	
-	
+
+
     class Meta:
         ordering = ["last_name", "first_name"]
-    
-    
+
+
     def full_name(self):
         return ("%s %s" % (
             self.first_name,
             self.last_name)).strip()
-    
+
     def __unicode__(self):
         return self.full_name()
-    
+
     def __repr__(self):
         return "%r (%r)" % (
             self.full_name(),
             self.alias)
-    
+
     def __json__(self):
         return {
             "pk":         self.pk,
@@ -91,8 +91,8 @@ class Reporter(models.Model):
             "first_name": self.first_name,
             "last_name":  self.last_name,
             "str":        unicode(self) }
-    
-    
+
+
     @classmethod
     def exists(klass, reporter, connection):
         """Checks if a reporter has already been entered into the system"""
@@ -115,29 +115,29 @@ class Reporter(models.Model):
             # if we couldn't find a connection then they 
             # don't exist
             return False 
-        
+
     @classmethod
     def parse_name(klass, flat_name):
         """Given a single string, this function returns a three-string
            tuple containing a suggested alias, first name, and last name,
            via some quite crude pattern matching."""
-        
+
         patterns = [
             # try a few common name formats.
             # this is crappy but sufficient
             r"([a-z]+)",                       # Adam
             r"([a-z]+)\s+([a-z]+)",            # Evan Wheeler
-            r"([a-z]+)\s+[a-z]+\.?\s+([a-z]+)",# Mark E. Johnston, Lee Harvey Oswald 
+            r"([a-z]+)\s+[a-z]+\.?\s+([a-z]+)",# Mark E. Johnston
             r"([a-z]+)\s+([a-z]+\-[a-z]+)"     # Erica Kochi-Fabian
         ]
-        
+
         def unique(str):
             """Checks an alias for uniqueness; if it is already taken, alter it
                (by append incrementing digits) until an available alias is found."""
-            
+
             n = 1
             alias = str.lower()
-            
+
             # keep on looping until an alias becomes available.
             # --
             # WARNING: this isn't going to work at high volumes, since the alias
@@ -147,55 +147,55 @@ class Reporter(models.Model):
             while klass.objects.filter(alias__iexact=alias).count():
                 alias = "%s%d" % (str.lower(), n)
                 n += 1
-            
+
             return alias
-        
+
         # try each pattern, returning as
         # soon as we find something that fits
         for pat in patterns:
             m = re.match("^%s$" % pat, flat_name, re.IGNORECASE)
             if m is not None:
                 g = m.groups()
-                
+
                 # return single names as-is
                 # they might already be aliases
                 if len(g) == 1:
                     alias = unique(g[0].lower())
                     return (alias, g[0], "")
-                    
+
                 else:
                     # return only the letters from
                     # the first and last names
                     alias = unique(g[0][0] + re.sub(r"[^a-zA-Z]", "", g[1]))
                     return (alias.lower(), g[0], g[1])
-        
+
         # we have no idea what is going on,
         # so just return the whole thing
         alias = unique(re.sub(r"[^a-zA-Z]", "", flat_name))
         return (alias.lower(), flat_name, "")
-    
-    
+
+
     def connection(self):
         """Returns the connection object last used by this Reporter.
            The field is (probably) updated by app.py when receiving
            a message, so depends on _incoming_ messages only."""
-        
+
         # TODO: add a "preferred" flag to connection, which then
         # overrides the last_seen connection as the default, here
         try:
             return self.connections.latest("last_seen")
-        
+
         # if no connections exist for this reporter (how
         # did that happen?!), then just return None...
         except PersistantConnection.DoesNotExist:
             return None
 
-    
+
     def last_seen(self):
         """Returns the Python datetime that this Reporter was last seen,
            on any Connection. Before displaying in the WebUI, the output
            should be run through the XXX  filter, to make it prettier."""
-        
+
         # comprehend a list of datetimes that this
         # reporter was last seen on each connection,
         # excluding those that have never seen them
@@ -203,7 +203,7 @@ class Reporter(models.Model):
             c.last_seen
             for c in self.connections.all()
             if c.last_seen is not None]
-        
+
         # return the latest, or none, if they've
         # has never been seen on ANY connection
         return max(timedates) if timedates else None
@@ -216,18 +216,19 @@ class PersistantBackend(models.Model):
        (in models which wish to link to a backend), since the
        available backends (and their orders) may change after
        deployment; hence, something persistant is needed."""
+
     slug  = models.CharField(max_length=30, unique=True)
     title = models.CharField(max_length=30)
-    
-    
+
+
     class Meta:
         verbose_name = "Backend"
-    
-    
+
+
     def __unicode__(self):
         return self.title
-    
-    
+
+
     @classmethod
     def from_message(klass, msg):
         """"Fetch a PersistantBackend object from the data buried in a rapidsms.message.Message
@@ -246,45 +247,46 @@ class PersistantConnection(models.Model):
        Reporter is seen communicating via a new backend, or is expected
        to do so in future, a PersistantConnection should be created,
        so they can be recognized by their backend + identity pair."""
+
     backend   = models.ForeignKey(PersistantBackend, related_name="connections")
     identity  = models.CharField(max_length=30)
     reporter  = models.ForeignKey(Reporter, related_name="connections", blank=True, null=True)
     last_seen = models.DateTimeField(blank=True, null=True)
-    
-    
+
+
     class Meta:
         verbose_name = "Connection"
         unique_together = ("backend", "identity")
-    
-    
+
+
     def __unicode__(self):
         return "%s:%s" % (
             self.backend,
             self.identity)
-    
+
     def __json__(self):
         return {
             "pk": self.pk,
             "identity": self.identity,
             "reporter": self.reporter,
             "str": unicode(self) }
-    
-    
+
+
     @classmethod
     def from_message(klass, msg):
         obj, created = klass.objects.get_or_create(
             backend  = PersistantBackend.from_message(msg),
             identity = msg.connection.identity)
-        
+
         if created:
             obj.save()
-        
+
         # just return the object. it doesn't matter
         # if it was created or fetched. TODO: maybe
         # a parameter to return the tuple
         return obj
-    
-    
+
+
     def seen(self):
         """"Updates the last_seen field of this object to _now_, and saves.
             Unless the linked Reporter has an explict preferred connection
@@ -292,8 +294,7 @@ class PersistantConnection(models.Model):
             it as the implicit default connection for the Reporter. """
         self.last_seen = datetime.now()
         return self.save()
-    
-    
+
     def prefer(self):
         """Removes the _preferred_ flag from all other PersistantConnection objects
            linked to the same Reporter, and sets the _preferred_ flag on this object."""
@@ -307,4 +308,3 @@ class PersistantConnection(models.Model):
            the Model and view layers, but the folks in #django don't have any
            better suggestions."""
         return "%s?connection=%s" % (reverse("add-reporter"), self.pk)
-
