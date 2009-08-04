d2f56e6808aeee86dce1dfd8760bb5f88841bbcc
adammck <adam.mckaig@gmail.com>
2009-08-04 11:44:14 -0400

    added form to add new location
    actually, the forms were there already, but now they're wired up in
    views.py to display properly. they don't actually SAVE yet, though.

--- locations/urls.py
+++ locations/urls.py
@@ -18,6 +18,14 @@ urlpatterns = patterns('',
         views.location_type,
         name="view-location-type"),
 
+    # add a location of a defined type (note that
+    # there's no url to define a location of an
+    # arbitrary type. since location types rarely
+    # change, i'm leaving that to the django admin)
+    url(r'^location-types/(?P<location_type_pk>\d+)/locations/add$',
+        views.add_location,
+        name="add-location"),
+
     # to view all of the data linked to a location,
     # which may have come from _any_ rapidsms app
     url(r'^location-types/(?P<location_type_pk>\d+)/locations/(?P<location_pk>\d+)$',
