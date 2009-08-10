5b6855e156b8736dfd8ae5201601700260015473
adammck <adam.mckaig@gmail.com>
2009-08-10 15:07:03 -0400

    added global AJAX pagination
    the helper plugs straight into the existing pagination stuff, and i've
    added rs-pagination.js globally, making this a free upgrade.
    
    this doesn't save any work on the server-side, since the entire page
    must still be regenerated (maybe that will come later), but it makes
    the UI a lot snappier. this is in preparation for the upgrades to the
    LOCATIONS app, which will preload ALL data in a paginatable table, to
    display it on a google map without showing a huge single table.

--- webui/templatetags/pagination.py
+++ webui/templatetags/pagination.py
@@ -10,23 +10,26 @@ register = template.Library()
 
 @register.inclusion_tag("webui/partials/paginator.html", takes_context=True)
 def paginator(context, objects):
-    
+
     prefix = getattr(objects, "prefix", "")
+    dom_id = prefix + "paginator"
     page_param = prefix + "page"
     req = context["request"]
-    
+
     def __link_to(page):
         kwargs = { page_param: page }
         return self_link(req, **kwargs)
-    
+
     return {
         "objects":         objects,
         "page_param":      page_param,
+        "dom_id":          dom_id,
+
         "first_page_link": __link_to(1),
         "prev_page_link":  __link_to(objects.previous_page_number()),
         "next_page_link":  __link_to(objects.next_page_number()),
         "last_page_link":  __link_to(objects.paginator.num_pages),
-        
+
         # django's template tags suck. we must also include the request
         # object in this context, because django doesn't pass along the
         # parent context (containing request.GET, from which we pluck
