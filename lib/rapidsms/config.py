97cf427648e84a6a2479c0c0a3f0b5a4274dcc33
adammck <adam.mckaig@gmail.com>
2009-08-18 21:16:41 -0400

    made app configs lazy, to allow db access in config.py
    this is more useful than it might sound. it allows the webui to be
    pre-configured based upon the contents of the database. there are
    various ways to do the same thing, but i think this is the cleanest,
    since it requires no special knowledge about rapidsms internals.
    
    read the docstring for rapidsms.config.LazyAppConf, and see the next
    commit for a little more info and my use-case.

--- lib/rapidsms/config.py
+++ lib/rapidsms/config.py
@@ -1,12 +1,54 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import os, log
+import os, log, collections
 from ConfigParser import SafeConfigParser
 
 
 def to_list (item, separator=","):
     return filter(None, map(lambda x: str(x).strip(), item.split(separator)))
+
+
+class LazyAppConf(collections.Mapping):
+    """RapidSMS apps can provide an optional config.py, which is like a per-app
+       version of Django's settings.py. The settings defined there are available
+       to both the RapidSMS router and the Django WebUI. This class acts very
+       much like a dict, but doesn't build the app config until it's really
+       required (much like a Django QuerySet).
+
+       This is almost completely useless, except it allows us to prepare the
+       Config opject (full of LazyAppConf instances) while building the Django
+       settings (rapidsms.webui.settings), _and_ allow app configs to hit the
+       database. If the configs were regular (eager) dicts, Django would refuse
+       to hit the database, because "You haven't set the DATABASE_ENGINE setting
+       yet"."""
+
+    def __init__(self, config, app_name):
+        self.app_name = app_name
+        self.config = config
+        self._cache = None
+
+    def _dict(self):
+        if self._cache is None:
+            self._cache = self.config.app_section(self.app_name)
+        return self._cache
+
+    # not strictly necessary, but avoids logging the useless
+    # "Added app: <rapidsms.config.LazyAppConf object at 0x......c>"
+    def __repr__(self):
+        return repr(self._dict())
+
+    # so long as these three methods are defined,
+    # collections.Mapping takes care of the rest.
+    # from the outside, it looks just like a dict
+    def __len__(self):
+        return len(self._dict())
+
+    def __getitem__(self, key):
+        return self._dict()[key]
+
+    def __iter__(self):
+        return iter(self._dict())
 
 
 class Config (object):
@@ -15,7 +57,7 @@ class Config (object):
     
     def __init__ (self, *paths):
         self.parser = SafeConfigParser()
-        
+
         # read the configuration, and store the list of
         # config files which were successfully parsed
         self.sources = self.parser.read(paths)
@@ -132,25 +174,33 @@ class Config (object):
                 (app_type, " or ".join(self.app_prefixes)))
 
 
-    def component_section (self, name):
-        
-        # fetch the current config for this section
-        # from raw_data (or default to an empty dict),
-        # then copy it, so we don't alter the original
-        data = self.raw_data.get(name, {}).copy()
-        
-        # although "name" and "type" are deliberately distinct (to enable multiple
-        # components of the same type to run concurrently), it's cumbersome to have
-        # to provide a type every single time, so default to the name
-        if not "type" in data:
-            data["type"] = name
-        
-        return data
+    def lazy_app_section (self, name):
+        return LazyAppConf(self, name)
 
 
     def app_section (self, name):
-        data = self.component_section(name)
-        
+
+        # fetch the current config for this app
+        # from raw_data (or default to an empty dict),
+        # then copy it, so we don't alter the original
+        data = self.raw_data.get(name, {}).copy()
+
+        # "type" is ONLY VALID FOR BACKENDS now. it's not [easily] possible
+        # to run multple django apps of the same type side-by-side, so i'm
+        # warning here to avoid confusion (and allow apps to be lazy loaded)
+        if "type" in data:
+            raise DeprecationWarning(
+                'The "type" option is not supported for apps. It does ' +\
+                'nothing, since running multple apps of the same type ' +\
+                'is not currently possible.')
+
+        # ...that said, the terms _type_ and _name_ are still mixed up
+        # in various places, so we must support both. another naming
+        # upheaval is probably needed to clear this up (or perhaps we
+        # should just scrap the shitty INI format, like we should have
+        # done in the first place to avoid this entire mess)
+        data["type"] = name
+
         # attempt to import the module, to locate it (it might be in ./apps,
         # contrib, or rapidsms/apps) and verify that it imports cleanly
         data["module"], module = self.__import_app(data["type"])
@@ -175,7 +225,19 @@ class Config (object):
 
 
     def backend_section (self, name):
-        return self.component_section(name)
+
+        # fetch the current config for this backend
+        # from raw_data (or default to an empty dict),
+        # then copy it, so we don't alter the original
+        data = self.raw_data.get(name, {}).copy()
+
+        # although "name" and "type" are deliberately distinct (to enable multiple
+        # backends of the same type to run concurrently), it's cumbersome to have
+        # to provide a type every single time, so default to the name
+        if not "type" in data:
+            data["type"] = name
+
+        return data
     
     
     def parse_rapidsms_section (self, raw_section):
@@ -185,10 +247,10 @@ class Config (object):
         app_names     = to_list(raw_section.get("apps",     ""))
         backend_names = to_list(raw_section.get("backends", ""))
         
-        # run lists of component names through component_section,
-        # to transform into lists of dicts containing more meta-info
-        return { "apps":     [self.app_section(n) for n in app_names],
-                 "backends": [self.backend_section(n) for n in backend_names] }
+        # run lists of component names through [app|backend]_section,
+        # to transform into dicts of dicts containing more meta-info
+        return { "apps":     dict((n, self.lazy_app_section(n)) for n in app_names),
+                 "backends": dict((n, self.backend_section(n)) for n in backend_names) }
 
 
     def parse_log_section (self, raw_section):
