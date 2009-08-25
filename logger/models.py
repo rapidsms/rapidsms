08a0bd8f6d39ea851514de8caf99261d01a7150b
adammck <adam.mckaig@gmail.com>
2009-08-25 17:28:05 -0400

    updated logger to use foreign keys
    i'm not sure why it wasn't doing this in the first place. maybe to avoid
    cascading delete. we already have a very low-level log (to file), and
    reporters and connections shouldn't be deleted anyway, so i think the
    extra functionality is well worth it.

--- logger/models.py
+++ logger/models.py
@@ -1,26 +1,61 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-import django
+
 from django.db import models
+from django.core.exceptions import ValidationError
+from apps.reporters.models import *
 
 
 class MessageBase(models.Model):
-    text = models.CharField(max_length=140)
-	# TODO save connection title rather than wacky object string?
-    identity = models.CharField(max_length=150)
-    backend = models.CharField(max_length=150)
-    
-    def __unicode__(self):
-        return "%s (%s) %s:" % (self.identity, self.backend, self.text)
-    
+    text = models.TextField()
+
     class Meta:
         abstract = True
-    
-    
+
+    def save(self, *args, **kwargs):
+        """Verifies that one (not both) of the reporter and connection fields
+           have been populated (raising ValidationError if not), and saves the
+           object as usual. Note that neither field is defined in this abstract
+           base class (because of the distinct related_names)."""
+
+        if (self.reporter or self.connection) is None:
+            raise ValidationError(
+                "A valid (not null) reporter or connection (but " +\
+                "not both) must be provided to save the object")
+
+        # all is well; save the object as usual
+        models.Model.save(self, *args, **kwargs)
+
+    @property
+    def who(self):
+        """Returns the Reporter or Connection linked to this object."""
+        return self.reporter or self.connection
+
+    def __unicode__(self):
+
+        # crop the text if it's long
+        # (to avoid exploding the admin)
+        if len(self.text) < 60: str = self.text
+        else: str = "%s..." % (self.text[0:57])
+
+        return "%s (%s %s)" %\
+            (str, self.prep, self.who)
+
+    def __repr__(self):
+        return "%s %r: %r" %\
+            (self.prep, self.who, self.text)
+
+
 class IncomingMessage(MessageBase):
-    received = models.DateTimeField(auto_now_add=True)
-    
+    connection = models.ForeignKey(PersistantConnection, null=True, related_name="incoming_messages")
+    reporter   = models.ForeignKey(Reporter, null=True, related_name="incoming_messages")
+    received   = models.DateTimeField(auto_now_add=True)
+    prep = "from"
+
 
 class OutgoingMessage(MessageBase):
-    sent = models.DateTimeField(auto_now_add=True)
+    connection = models.ForeignKey(PersistantConnection, null=True, related_name="outgoing_messages")
+    reporter   = models.ForeignKey(Reporter, null=True, related_name="outgoing_messages")
+    sent       = models.DateTimeField(auto_now_add=True)
+    prep = "to"
