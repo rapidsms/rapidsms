66052a050bfae4ed8db4f742b6292280e6cd5063
adammck <adam.mckaig@gmail.com>
2009-09-04 09:37:09 -0400

    added i18n app
    provides a sensible, if wildly inefficient (but ripe for caching) way to
    internationalize apps, without any dependancies. it currently uses a big
    dict for the base translations (not gettext), which can be overridden by
    records in the database. i'll whip up a webui to make that friendly for
    translators, next.
    
    i chose to use both the database AND flat files, so we can easily share
    the translations via git[hub] (like gettext), while end-users can easily
    customize them via the webui. it would be nice to export those objects
    as a locale.py file -- perhaps that will come later.
    
    most i18n apps would actually depend upon the reporters app to store
    the preferred languages, but here's the simplest example i can think of.
    it uses the caller's identity (via msg.peer) to store their preference.
    
      locale.py
      ---------
    
        LOCALE = {
          "en": {
            "welcome": "Hello!",
            "error": "I don't understand" },
          "fr": {
            "welcome": "Bonjour!",
            "error": "Je ne understande" }}
    
      app.py
      ------
    
        from apps.i18n.app import InternationalApp
    
        class App(InternationalApp):
          def start(self):
            self.langs = {}
    
          def handle(self, msg):
    
            # capture "LANG [CODE]" messages, to set this
            # caller's preference for future messages
            if msg[0:5] == "lang ":
              self.lang[msg.peer] = msg[5:].lower()
              msg.respond(self.lang[msg.peer], "welcome")
              return True
    
           # capture all other messages, and respond with
           # an error in the caller's preferred language
           msg.respond(self.lang[msg.peer], "error")
           return True
    
      sample sms session
      ------------------
    
        # english is the default
        ~~> hello
        <~~ I don't understand
    
        # set the language and trigger some errors
        ~~> lang fr
        <~~ Bonjour!
        ~~> what is mudkips?
        <~~ Je ne understande
        ~~> sorry, what?
        <~~ Je ne understande
    
        # change the language, trigger more errors
        ~~> lang en
        <~~ Hello!
        ~~> what is mudkips?
        <~~ I don't understand
    
        # set the language to something that doesn't exist,
        # and trigger more errors. (fallback to english)
        ~~> lang xyz
        <~~ Hello!
        ~~> that wasn't in XYZ
        <~~ I don't understand
    
    this one actually has pretty good docs and some scripted unit tests, so
    it's probably best to look there for a better idea what this thing does.

---
+++ i18n/locale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+LOCALE = {
+    "en": {
+        "a": "Alpha",
+        "b": "Beta",
+        "g": "Gamma"
+    },
+    "fr": {
+        "a": "ahplA",
+        "b": "ateB"
+    }
+}
