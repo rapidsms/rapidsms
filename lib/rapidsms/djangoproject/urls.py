90ee551cc8f2803aa2a046b7ab36264c0ff36228
adammck <adam.mckaig@gmail.com>
2009-09-13 16:42:27 -0400

    simplified urls.py with utils.modules stuff

--- lib/rapidsms/djangoproject/urls.py
+++ lib/rapidsms/djangoproject/urls.py
@@ -4,7 +4,8 @@
 
 import os, sys
 from rapidsms.djangoproject import settings
-from django.conf.urls.defaults import *
+from rapidsms.utils.modules import try_import, get_module_path
+from django.conf.urls.defaults import patterns, url
 
 
 # this list will be populated with the
@@ -14,50 +15,29 @@ from django.conf.urls.defaults import *
 urlpatterns = []
 
 
-# iterate each of the active rapidsms apps (from the ini),
-# and (attempt to) import the urls.py from each. it's okay
-# if this fails, since not all apps have a webui
-for rs_app in settings.RAPIDSMS_APPS.keys():
-    try:
-    
-        # import the single "urlpatterns" attribute
-        package_name = "%s.urls" % (rs_app)
-        module = __import__(package_name, {}, {}, ["urlpatterns"])
+for module_name in settings.RAPIDSMS_APPS.keys():
 
-        # add the explicitly defined urlpatterns
-        urlpatterns += module.urlpatterns
+    # attempt to import this app's urls
+    module = try_import("%s.urls" % (module_name))
+    if module is None: continue
 
-        # does urls.py have a sibling "static" dir?
-        mod_dir = os.path.dirname(module.__file__)
-        static_dir = "%s/static" % mod_dir
-        if os.path.exists(static_dir):
+    # add the explicitly defined urlpatterns
+    urlpatterns += module.urlpatterns
 
-            # found a static dir, so automatically serve those files
-            # via django. this is frowned upon in production, since
-            # the server isn't tough (or fast), but there are so many
-            # places that static files can come from, i'm not sure how
-            # we would auto-configure that in apache. maybe we could
-            # extend manager.py, to output an http conf mapping all
-            # of this stuff for apache?
-            urlpatterns += patterns("", url(
-                "^static/%s/(?P<path>.*)$" % rs_app,
-                "django.views.static.serve",
-                {"document_root": static_dir }
-            ))
-    
-    # urls.py couldn't be imported for this app...
-    # was it because importing XXX.urls failed, or
-    # because something INSIDE urls.py raised?
-    except ImportError:
-        
-        # extract a backtrace, so we can find
-        # out where the exception was raised
-        tb = sys.exc_info()[2]
-        
-        # if there is a NEXT frame, it means that the __import__
-        # statement in this file didn't fail -- the exception was
-        # raised from within the imported urls.py. it's important
-        # that we allow this error to propagate, to avoid silently
-        # masking the error!
-        if tb.tb_next:
-            raise
+    # does urls.py have a sibling "static" dir?
+    module_path = os.path.dirname(module.__file__)
+    static_dir = "%s/static" % module_path
+    if os.path.exists(static_dir):
+
+        # found a static dir, so automatically serve those files
+        # via django. this is frowned upon in production, since
+        # the server isn't tough (or fast), but there are so many
+        # places that static files can come from, i'm not sure how
+        # we would auto-configure that in apache. maybe we could
+        # extend manager.py, to output an http conf mapping all
+        # of this stuff for apache?
+        urlpatterns += patterns("", url(
+            "^static/%s/(?P<path>.*)$" % module_name,
+            "django.views.static.serve",
+            {"document_root": static_dir}
+        ))
