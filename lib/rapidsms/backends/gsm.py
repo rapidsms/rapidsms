f27b936f1ee426d9be9359137ec07042f00abd72
adammck <adam.mckaig@gmail.com>
2009-09-18 06:18:03 -0400

    added wait_for_modem and retry loop to gsm backend
    some apps (like the training app) send messages during app startup,
    which can occur before the modem is initialized (since the backends
    start up asycnhronously in their own threads. this method blocks until
    the modem is available (with a timeout), so the apps dont have to check.

--- lib/rapidsms/backends/gsm.py
+++ lib/rapidsms/backends/gsm.py
@@ -10,13 +10,16 @@ from rapidsms.messages.incoming import IncomingMessage
 from rapidsms.backends import Backend as BackendBase
 
 
-# number of seconds to wait between
-# polling the modem for incoming SMS
-POLL_INTERVAL = 10
-
-
 class Backend(BackendBase):
     _title = "pyGSM"
+
+    # number of seconds to wait between
+    # polling the modem for incoming SMS
+    POLL_INTERVAL = 10
+
+    # time to wait for the start method to
+    # complete before assuming that it failed
+    MAX_CONNECT_TIME = 10
 
 
     def configure(self, **kwargs):
@@ -33,12 +36,48 @@ class Backend(BackendBase):
         self.modem = None
 
 
+    def __str__(self):
+        return self._title
+
+
+    def _wait_for_modem(self):
+        """
+        Blocks until this backend has connected to and initialized the modem,
+        waiting for a maximum of self.MAX_CONNECT_TIME (default=10) seconds.
+        Returns true when modem is ready, or false if it times out.
+        """
+
+        # if the modem isn't present yet, this message is probably being sent by
+        # an application during startup from the main thread, before this thread
+        # has connected to the modem. block for a short while before giving up.
+        for n in range(0, self.MAX_CONNECT_TIME*10):
+            if self.modem is not None: return True
+            time.sleep(0.1)
+
+        # timed out. we're still not connected
+        # this is bad news, but not fatal, so warn
+        self.warning("Timed out while waiting for modem")
+        return False
+
+
     def send(self, message):
-        self.sent_messages += 1
-        
-        self.modem.send_sms(
+
+        # if this message is being sent from the start method of
+        # an app (which is run in the main thread), this backend
+        # may not have had time to start up yet. wait for it
+        if not self._wait_for_modem():
+            return False
+
+        # attempt to send the message
+        # failure is bad, but not fatal
+        was_sent = self.modem.send_sms(
             str(message.connection.identity),
             message.text)
+
+        if was_sent: self.sent_messages += 1
+        else:        self.failed_messages += 1
+
+        return was_sent
 
 
     def gsm_log(self, modem, str, level):
@@ -46,6 +85,12 @@ class Backend(BackendBase):
 
 
     def status(self):
+
+        # abort if the modem isn't connected
+        # yet. there's no status to fetch
+        if not self._wait_for_modem():
+            return None
+
         csq = self.modem.signal_strength()
 
         # convert the "real" signal
@@ -86,20 +131,21 @@ class Backend(BackendBase):
             # wait for POLL_INTERVAL seconds before continuing
             # (in a slightly bizarre way, to ensure that we abort
             # as soon as possible when the backend is asked to stop)
-            for n in range(0, POLL_INTERVAL):
+            for n in range(0, self.POLL_INTERVAL*10):
                 if not self.running: return None
-                time.sleep(1)
+                time.sleep(0.1)
 
 
     def start(self):
         self.sent_messages = 0
+        self.failed_messages = 0
         self.received_messages = 0
 
         self.modem = pygsm.GsmModem(
             logger=self.gsm_log,
             **self.modem_kwargs)
 
-        # If we connected successfully to the modem, call
+        # if we connected successfully to the modem, call
         # superclass to start the run loop -- it just sets
         # self._running to True and calls run.
         if self.modem is not None:
@@ -107,7 +153,7 @@ class Backend(BackendBase):
 
 
     def stop(self):
-        
+
         # call superclass to stop--sets self._running
         # to False so that the 'run' loop will exit cleanly.
         BackendBase.stop(self)
