2146a42b8dd82d150cd6697ff9f3d07983751da4
adammck <adam.mckaig@gmail.com>
2009-10-09 03:41:28 -0400

    replaced rapidsms.tests.__init__ magic with a custom suite
    i was manually keeping __test__ up to date before, which was a bit janky
    and surprising, so i refactored it properly. now all of the modules that
    we wish to import unit tests from are simply imported. both regular unit
    tests and doctests are run every time './rapidsms test' is started.

--- lib/rapidsms/tests/__init__.py
+++ lib/rapidsms/tests/__init__.py
@@ -2,29 +2,55 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-# import all of the tests in this package, so they're
-# all run when this app (rapidsms) (yes, rapidsms is
-# a django app now) is tested via './rapidsms test'
-from .test_config        import *
-from .test_component     import *
-from .test_app           import *
-from .test_backend       import *
-from .test_backend_irc   import *
-from .test_backend_spomc import *
-from .test_log           import *
-#from .test_message       import *
-from .test_router        import *
+import unittest, doctest
 
 
-# import a few classes that have doctests attached
-# to them, so they're also run via './rapidsms test'
-from ..router import Router
-from ..app import App
+def suite():
+    """
+    Returns an extended test suite for this module, which includes all of the
+    test modules in this package (rapidsms.tests), along with important parts of
+    RapidSMS which aren't usually tested. (Django only runs tests declared in
+    models.py and tests.py as default. imported modules are ignored.)
+    """
 
-# define which classes to test. this sort of sucks, since they're fun
-# via this module, which means that doctest (at least, the _doctest.py
-# bundled with django) can't find the line numbers when test fail
-__test__ = {
-    'Router': Router,
-    'App': App
-}
+    # run all of the tests in this directory
+    from . import test_config
+    from . import test_component
+    from . import test_backend
+    from . import test_backend_irc
+    from . import test_backend_spomc
+    from . import test_app
+    from . import test_log
+    #from . import test_message
+    from . import test_router
+
+    # import various other modules,
+    # to find and run their doctests
+    from .. import router, app
+
+    # start with an empty test suite
+    _suite = unittest.TestSuite()
+
+    # iterate all of the modules that we
+    # just imported, looking for tests
+    l = locals()
+    for name in l:
+
+        # ignore privates
+        if name.startswith("_"):
+            continue
+
+        # load regular unit tests from 'name'
+        _suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(
+            l[name]))
+
+        # check for doctests, too. ValueError is
+        # raised if none are found; no big deal
+        try:
+            _suite.addTest(doctest.DocTestSuite(
+                l[name]))
+
+        except ValueError:
+            pass
+
+    return _suite
