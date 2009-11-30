5d5c6a1fccc04961c3a13280281cdbd7209f29f4
adammck <adam.mckaig@gmail.com>
2009-11-29 22:34:35 -0500

    cleaned up the webui app a bit

--- webui/urls.py
+++ webui/urls.py
@@ -2,10 +2,8 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 from django.conf.urls.defaults import *
-import views
+from . import views
 
 urlpatterns = patterns('',
-    url(r'^$',     views.dashboard),
-    url(r'^ping$', views.check_availability),
+    url(r'^$', views.dashboard)
 )
-
--- webui/views.py
+++ webui/views.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-from django.http import HttpResponse
 from rapidsms.djangoproject.utils import render_to_response
-from django.views.decorators.cache import cache_page
 
-def check_availability(req):
-    return HttpResponse("OK")
-
-@cache_page(60 * 15)
 def dashboard(req):
 	return render_to_response(req, "dashboard.html")
