5763620c7f12ffa239df17c769f78ea0aef34a22
adammck <adam.mckaig@gmail.com>
2009-12-13 15:23:19 +0300

    moved ajax app to contrib

--- ajax/config.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-
-title = "AJAX Helper"
-host = "localhost"
-port = 8001
--- ajax/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-
-from django.conf.urls.defaults import *
-import ajax.views as views
-
-urlpatterns = patterns('',
-    (r'^ajax/(?P<path>.*)$', views.proxy),
-)
