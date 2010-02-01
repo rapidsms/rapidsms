7532327bbfeb04023a867136fc1a38f8f103466d
adammck <adam.mckaig@gmail.com>
2010-02-01 03:09:12 -0500

    added the djangoadmin app to the default settings.py

--- settings.py
+++ settings.py
@@ -12,7 +12,16 @@ DATABASE_NAME   = "/tmp/rapidsms.sqlite3"
 INSTALLED_APPS = [
     "rapidsms",
     "rapidsms.contrib.handlers",
-    "rapidsms.contrib.echo"
+    "rapidsms.contrib.echo",
+
+    # enable the django admin using a little shim app (which includes
+    # the required urlpatterns), and a bunch of undocumented apps that
+    # the AdminSite seems to explode without
+    "django.contrib.auth",
+    "django.contrib.admin",
+    'django.contrib.sessions',
+    "django.contrib.contenttypes",
+    "rapidsms.contrib.djangoadmin"
 ]
 
 
