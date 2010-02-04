94a31ef72b4dc9b9ac0a5b431716af7ea6da05cc
adammck <adam.mckaig@gmail.com>
2010-02-03 19:24:29 -0500

    don't auto-url-map django contrib apps

--- lib/rapidsms/djangoproject/urls.py
+++ lib/rapidsms/djangoproject/urls.py
@@ -8,18 +8,22 @@ from django.conf.urls.defaults import patterns, url
 from rapidsms.utils.modules import try_import
 
 
-# this list will be populated with the
-# urls from the urls.urlpatterns of each
-# running rapidsms app, then imported by
-# django as if we'd declared them all here
+# this list will be populated with the urls from the urls.urlpatterns of
+# each installed app, then found by django as if we'd listed them here.
 urlpatterns = []
 
 
 for module_name in settings.INSTALLED_APPS:
 
+    # leave django contrib apps alone. (many of them include urlpatterns
+    # which shouldn't be auto-mapped.) this is a hack, but i like the
+    # automatic per-app mapping enough to keep it. (for now.)
+    if module_name.startswith("django."):
+        continue
+
     # attempt to import this app's urls
     module = try_import("%s.urls" % (module_name))
-    if module is None: continue
+    if not hasattr(module, "urlpatterns"): continue
 
     # add the explicitly defined urlpatterns
     urlpatterns += module.urlpatterns
@@ -30,19 +34,21 @@ for module_name in settings.INSTALLED_APPS:
     # these files should be served directly
     if settings.DEBUG:
         if not settings.MEDIA_URL.startswith("http://"):
+
             media_prefix = settings.MEDIA_URL.strip("/")
+            module_suffix = module_name.split(".")[-1]
 
-        # does urls.py have a sibling "static" dir? (media is always
-        # served from "static", regardless of what MEDIA_URL says)
-        module_path = os.path.dirname(module.__file__)
-        static_dir = "%s/static" % (module_path)
-        if os.path.exists(static_dir):
+            # does urls.py have a sibling "static" dir? (media is always
+            # served from "static", regardless of what MEDIA_URL says)
+            module_path = os.path.dirname(module.__file__)
+            static_dir = "%s/static" % (module_path)
+            if os.path.exists(static_dir):
 
-            # map to {{ MEDIA_URL }}/appname
-            urlpatterns += patterns("", url(
-                "^%s/%s/(?P<path>.*)$" % (
-                    media_prefix,
-                    module_name),
-                "django.views.static.serve",
-                {"document_root": static_dir}
-            ))
+                # map to {{ MEDIA_URL }}/appname
+                urlpatterns += patterns("", url(
+                    "^%s/%s/(?P<path>.*)$" % (
+                        media_prefix,
+                        module_suffix),
+                    "django.views.static.serve",
+                    {"document_root": static_dir}
+                ))
