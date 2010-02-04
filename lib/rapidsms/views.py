fccaba9e3912ad3130bd6782095dd1f81093a372
adammck <adam.mckaig@gmail.com>
2010-02-03 19:25:57 -0500

    move rapidsms.djangoproject.utils to rapidsms.utils
    the 'djangoproject' module should contain very little -- only just
    enough to make it a valid project. eventually, it should disappear,
    because a rapidsms project should simply *be* a django project.

--- lib/rapidsms/views.py
+++ lib/rapidsms/views.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-from rapidsms.djangoproject.utils import render_to_response
+
+from rapidsms.utils import render_to_response
+
 
 def dashboard(req):
 	return render_to_response(req, "dashboard.html")
