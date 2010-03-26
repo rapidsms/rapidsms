6df89d18133304b00ba86da1a26b093fcfed5892
adammck <adam.mckaig@gmail.com>
2010-03-26 18:51:05 -0400

    added very (too) simple RegisterHandler to registration app.

---
+++ registration/handlers/register.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from rapidsms.contrib.handlers import KeywordHandler
+from rapidsms.models import Contact
+
+
+class RegisterHandler(KeywordHandler):
+    """
+    """
+
+    keyword = "register|reg|join"
+
+    def help(self):
+        self.respond("To register, send JOIN <NAME>")
+
+    def handle(self, text):
+        contact = Contact.objects.create(
+            name=text)
+
+        self.respond(
+            "Thank you for registering, %(name)s!",
+            name=contact.name)
