843a23b97d79780b9a7d039a5d31a45a0046dbb3
adammck <adam.mckaig@gmail.com>
2010-04-11 02:23:49 -0400

    added tables_tags, for rendering generic tables.
    the actual table classes are still in contrib/messagelog. as noted
    there, this is based on andy mckay's django-reusable-tables.

---
+++ lib/rapidsms/templatetags/tables_tags.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from django import template
+register = template.Library()
+
+
+@register.inclusion_tag("rapidsms/templatetags/tables/colgroup.html")
+def render_colgroup(table):
+    return { "table": table }
+
+
+@register.inclusion_tag("rapidsms/templatetags/tables/thead.html")
+def render_thead(table):
+    return { "table": table }
+
+
+@register.inclusion_tag("rapidsms/templatetags/tables/tbody.html")
+def render_tbody(table):
+    return { "table": table }
+
+
+@register.inclusion_tag("rapidsms/templatetags/tables/tfoot.html")
+def render_tfoot(table):
+    return { "table": table }
+
+
+@register.simple_tag
+def render_cell(table, column, row):
+    return table.render_cell(column, row)
