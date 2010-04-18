92d33ebd0c3adc20e47e514d8acb9930b2f0d2f0
adammck <adam.mckaig@gmail.com>
2010-04-17 23:28:52 -0400

    expose the django_tables stuff slightly differently.

--- lib/rapidsms/tables.py
+++ lib/rapidsms/tables.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+from django_tables.models import BoundModelRow
+from django_tables.base import BoundRow
 from django_tables import *
+
+# i don't much like the names of these classes in django_tables, so i'm
+# providing aliases here. the fact that they're "bound" is an detail
+# which doesn't need to be exposed by the public API.
+ModelRow = BoundModelRow
+Row = BoundRow
