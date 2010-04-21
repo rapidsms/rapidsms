e3ad0aab9bc55252219390d9443237007858b4a7
Cory Zue <czue@dimagi.com>
2010-04-21 16:24:04 +0200

    removing register_tab import

--- locations/views.py
+++ locations/views.py
@@ -7,7 +7,6 @@ from .models import *
 from django.core.urlresolvers import reverse
 
 from django.shortcuts import get_object_or_404
-from django.templatetags.tabs_tags import register_tab
 from django.views.decorators.http import require_GET, require_http_methods
 from rapidsms.utils import render_to_response, web_message
 
