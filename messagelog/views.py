0f3303ce4650a3e9f9a7503274b0c8a8477c4991
Tobias McNulty <tobias@caktusgroup.com>
2010-05-06 12:53:00 +0200

    add pagination for message log app

--- messagelog/views.py
+++ messagelog/views.py
@@ -2,12 +2,19 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
+from django.core.paginator import QuerySetPaginator
 from rapidsms.utils import render_to_response
 from .tables import MessagelogTable
 
 
+MESSAGES_PER_PAGE = 25
+
+
 def message_log(req):
+    message_table = MessagelogTable(request=req)
+    message_table.paginate(QuerySetPaginator, MESSAGES_PER_PAGE,
+                           page=req.GET.get('page', 1))
     return render_to_response(req,
         "messagelog/index.html", {
-            "messages": MessagelogTable(request=req)
+            "messages": message_table,
         })
