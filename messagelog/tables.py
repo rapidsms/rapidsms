5ed6483c63a920f55381a1f7ae7815c3310eb369
adammck <adam.mckaig@gmail.com>
2010-06-14 23:58:24 -0400

    updated messagelog to new djangotables.
    this is a temporary fix, since modeltables are supposed to make this
    even easier, but they're broken at the moment.

--- messagelog/tables.py
+++ messagelog/tables.py
@@ -2,13 +2,20 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-from rapidsms import tables
+from djangotables import Table, Column
 from .models import Message
 
 
-class MessagelogTable(tables.ModelTable):
-    
+class MessageTable(Table):
+
+    # this is temporary, until i fix ModelTable!
+    contact = Column()
+    connection = Column()
+    direction = Column()
+    date = Column()
+    text = Column()
+
     class Meta:
-        model = Message
-        exclude = ['id']
+        #model = Message
+        #exclude = ['id']
         order_by = '-date'
--- messagelog/views.py
+++ messagelog/views.py
@@ -2,19 +2,13 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-from django.core.paginator import QuerySetPaginator
 from rapidsms.utils import render_to_response
-from .tables import MessagelogTable
-
-
-MESSAGES_PER_PAGE = 100
+from .tables import MessageTable
+from .models import Message
 
 
 def message_log(req):
-    message_table = MessagelogTable(request=req)
-    message_table.paginate(QuerySetPaginator, MESSAGES_PER_PAGE,
-                           page=req.GET.get('page', 1))
     return render_to_response(req,
         "messagelog/index.html", {
-            "messages": message_table,
+            "messages_table": MessageTable(Message.objects.all(), request=req)
         })
