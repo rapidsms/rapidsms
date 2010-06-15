74b9dec4eecd182e5a1bd2a1472506d93740cb96
adammck <adam.mckaig@gmail.com>
2010-06-15 00:25:57 -0400

    updated sample settings.py with more contrib apps and TABS.

--- settings.py
+++ settings.py
@@ -16,31 +16,42 @@ INSTALLED_APPS = [
     "rapidsms.contrib.handlers",
     "rapidsms.contrib.echo",
     "rapidsms.contrib.ajax",
+
     "rapidsms.contrib.httptester",
     "rapidsms.contrib.registration",
+    "rapidsms.contrib.messagelog",
+    "rapidsms.contrib.messaging",
+    "rapidsms.contrib.locations",
 
     # enable the django admin using a little shim app (which includes
     # the required urlpatterns), and a bunch of undocumented apps that
     # the AdminSite seems to explode without
     "django.contrib.auth",
     "django.contrib.admin",
-    'django.contrib.sessions',
+    "django.contrib.sessions",
     "django.contrib.contenttypes",
     "rapidsms.contrib.djangoadmin"
 ]
 
-# These apps should not be started by rapidsms in your tests
-# However the models + bootstrap will still be available through
-# django
+TABS = [
+    ("rapidsms.views.dashboard",                            "Dashboard"),
+    ("rapidsms.contrib.httptester.views.generate_identity", "Message Tester"),
+    ("rapidsms.contrib.registration.views.registration",    "Registration"),
+    ("rapidsms.contrib.messagelog.views.message_log",       "Message Log"),
+    ("rapidsms.contrib.messaging.views.messaging",          "Messaging"),
+    ("rapidsms.contrib.locations.views.dashboard",          "Map")
+]
+
+# these apps should not be started by rapidsms in your tests, however,
+# the models and bootstrap will still be available through django.
 TEST_EXCLUDED_APPS = (
     "django.contrib.sessions",
     "django.contrib.contenttypes",
     "django.contrib.auth",
     "rapidsms",
-    "rapidsms.contrib.ajax", 
-    "rapidsms.contrib.httptester", 
+    "rapidsms.contrib.ajax",
+    "rapidsms.contrib.httptester",
 )
-
 
 # the INSTALLED_BACKENDS setting is intended to resemble django 1.2's
 # DATABASE: http://docs.djangoproject.com/en/dev/ref/settings/#databases
@@ -58,6 +69,6 @@ INSTALLED_BACKENDS = {
     }
 }
 
-# after login, django redirects to this URL
-# rather than the default 'accounts/profile'
-LOGIN_REDIRECT_URL='/'
+# after login (which is handled by django.contrib.auth), redirect to the
+# dashboard rather than 'accounts/profile' (the default).
+LOGIN_REDIRECT_URL = "/"
