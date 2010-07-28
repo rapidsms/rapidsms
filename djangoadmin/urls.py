d7e37a6596c83f62db0033de12361c4e06f2c120
adammck <adam.mckaig@gmail.com>
2010-07-28 06:05:32 -0400

    fix djangoadmin deprecation warning.

--- djangoadmin/urls.py
+++ djangoadmin/urls.py
@@ -2,10 +2,10 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    (r'^admin/(.*)', admin.site.root))
+    (r'^admin/(.*)', include(admin.site.urls)))
