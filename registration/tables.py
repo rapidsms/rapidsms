c125c5db20ffda5401fae2e7d5ff29bf18499c10
adammck <adam.mckaig@gmail.com>
2010-07-28 05:48:49 -0400

    updated registration app.
    just compatibility. things had shifted a bit from under it.

--- registration/tables.py
+++ registration/tables.py
@@ -2,12 +2,19 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
+from django.core.urlresolvers import reverse
 from djangotables import Table, Column
 from rapidsms.models import Contact
 
 
+def _edit_link(cell):
+    return reverse(
+        "registration_edit",
+        args=[cell.row.pk])
+
+
 class ContactTable(Table):
-    alias    = Column()
+    alias    = Column(link=_edit_link)
     name     = Column()
     identity = Column()
 
