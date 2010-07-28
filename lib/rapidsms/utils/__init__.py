707a280cc037368348d82125222c159fa29224dc
adammck <adam.mckaig@gmail.com>
2010-07-28 09:54:13 -0400

    removed render_to_response wrapped. yay.

--- lib/rapidsms/utils/__init__.py
+++ lib/rapidsms/utils/__init__.py
@@ -2,6 +2,5 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-from .render_to_response import render_to_response
 from .pagination import paginated
 from .web_message import web_message
--- lib/rapidsms/views.py
+++ lib/rapidsms/views.py
@@ -2,19 +2,23 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.views.decorators.http import require_GET
 from django.contrib.auth.views import login as django_login
 from django.contrib.auth.views import logout as django_logout
-from rapidsms.utils import render_to_response
+
 
 @require_GET
 def dashboard(req):
-    return render_to_response(req, "dashboard.html")
+    return render_to_response(
+        "dashboard.html",
+        context_instance=RequestContext(req))
+
 
 def login(req, template_name="rapidsms/login.html"):
-    '''Login to rapidsms'''
     return django_login(req, **{"template_name" : template_name})
 
+
 def logout(req, template_name="rapidsms/loggedout.html"):
-    '''Logout of rapidsms'''
     return django_logout(req, **{"template_name" : template_name})
