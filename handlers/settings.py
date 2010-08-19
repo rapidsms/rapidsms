f3c636f3fac1137b86597f5e496f8c6edb682122
adammck <adam.mckaig@gmail.com>
2010-08-19 13:20:01 -0400

    fixed settings corruption by the handlers tests.
    the test temporarily fudges the settings file, to provide a predictable
    environment. if it failed, the settings weren't restored, breaking all
    subsequent tests. whoops!

--- handlers/settings.py
+++ handlers/settings.py
@@ -2,4 +2,4 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 INSTALLED_HANDLERS = None
-EXCLUDED_HANDLERS = ['what']
+EXCLUDED_HANDLERS = []
--- handlers/tests.py
+++ handlers/tests.py
@@ -20,32 +20,34 @@ def test_get_handlers():
     settings.INSTALLED_HANDLERS = None
     settings.EXCLUDED_HANDLERS  = None
 
-    assert_equal(get_handlers(), [])
+    try:
+        assert_equal(get_handlers(), [])
 
-    # this crappy test depends upon the ``echo`` contrib app, which
-    # defines exactly two handlers. i don't have a cleaner solution.
-    settings.INSTALLED_APPS = ['rapidsms.contrib.echo']
-    from rapidsms.contrib.echo.handlers.echo import EchoHandler
-    from rapidsms.contrib.echo.handlers.ping import PingHandler
+        # this crappy test depends upon the ``echo`` contrib app, which
+        # defines exactly two handlers. i don't have a cleaner solution.
+        settings.INSTALLED_APPS = ['rapidsms.contrib.echo']
+        from rapidsms.contrib.echo.handlers.echo import EchoHandler
+        from rapidsms.contrib.echo.handlers.ping import PingHandler
 
-    # check that both handlers were found as default
-    assert_equal(get_handlers(), [EchoHandler, PingHandler])
+        # check that both handlers were found as default
+        assert_equal(get_handlers(), [EchoHandler, PingHandler])
 
-    # exclude no handlers, explicitly include a single handler
-    settings.INSTALLED_HANDLERS = ['rapidsms.contrib.echo.handlers.ping']
-    assert_equal(get_handlers(), [PingHandler])
-    settings.INSTALLED_HANDLERS = []
+        # exclude no handlers, explicitly include a single handler
+        settings.INSTALLED_HANDLERS = ['rapidsms.contrib.echo.handlers.ping']
+        assert_equal(get_handlers(), [PingHandler])
+        settings.INSTALLED_HANDLERS = []
 
-    # exclude a single handler
-    settings.EXCLUDED_HANDLERS = ['rapidsms.contrib.echo.handlers.ping']
-    assert_equal(get_handlers(), [EchoHandler])
+        # exclude a single handler
+        settings.EXCLUDED_HANDLERS = ['rapidsms.contrib.echo.handlers.ping']
+        assert_equal(get_handlers(), [EchoHandler])
 
-    # exclude all handlers from the echo app
-    settings.EXCLUDED_HANDLERS = ['rapidsms.contrib.echo']
-    assert_equal(get_handlers(), [])
+        # exclude all handlers from the echo app
+        settings.EXCLUDED_HANDLERS = ['rapidsms.contrib.echo']
+        assert_equal(get_handlers(), [])
 
-    # restore pre-test settings.
-    settings.INSTALLED_APPS,
-    settings.INSTALLED_HANDLERS,
-    settings.EXCLUDED_HANDLERS = _settings
+    # always restore pre-test settings.
+    finally:
+        settings.INSTALLED_APPS,\
+        settings.INSTALLED_HANDLERS,\
+        settings.EXCLUDED_HANDLERS = _settings
 
