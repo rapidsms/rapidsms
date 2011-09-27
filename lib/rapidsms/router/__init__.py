2b1521231f3ef52bdcac96ea098c4f37e2891267
Tobias McNulty <tobias@caktusgroup.com>
2011-09-27 09:49:38 -0400

    add get_router() method to return the currently set router class based on the RAPIDSMS_ROUTER setting

--- lib/rapidsms/router/__init__.py
+++ lib/rapidsms/router/__init__.py
@@ -1,9 +1,35 @@
-#from .legacy import LegacyRouter as Router
-from .blocking import BlockingRouter as Router
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.importlib import import_module
 
+
+def get_router():
+    """
+    Imports and returns the router class described by the RAPIDSMS_ROUTER
+    setting, where RAPIDSMS_ROUTER is the full Python path to the class.
+    """
+    import_path = getattr(settings, 'RAPIDSMS_ROUTER',
+                          'rapidsms.router.legacy.LegacyRouter')
+    try:
+        dot = import_path.rindex('.')
+    except ValueError:
+        raise ImproperlyConfigured("%s isn't a Python path." % import_path)
+    module, classname = import_path[:dot], import_path[dot + 1:]
+    try:
+        mod = import_module(module)
+    except ImportError, e:
+        raise ImproperlyConfigured('Error importing module %s: "%s"' %
+                                   (module, e))
+    try:
+        return getattr(mod, classname)
+    except AttributeError:
+        raise ImproperlyConfigured('Module "%s" does not define a "%s" '
+                                   'class.' % (module, classname))
+
+# TODO: remove this global singleton
 # a single instance of the router singleton is available globally, like
 # the db connection. it shouldn't be necessary to muck with this very
 # often (since most interaction with the Router happens within an App or
 # Backend, which have their own .router property), but when it is, it
 # should be done via this process global
-router = Router()
+router = get_router()()
