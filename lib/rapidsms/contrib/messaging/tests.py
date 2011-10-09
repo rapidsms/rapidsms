f19b70a181ac9e5691f23b86bbf4bd827fe80932
Colin Copeland <copelco@caktusgroup.com>
2011-10-09 16:59:56 -0400

    convert tabs to spaces; update MessagingTest to use new CustomRouter setup

--- lib/rapidsms/contrib/messaging/tests.py
+++ lib/rapidsms/contrib/messaging/tests.py
@@ -51,98 +51,97 @@ class CreateDataTest(object):
 
 class SimpleBackend(BackendBase):
 
-	def __init__(self, *args, **kwargs):
-		self._saved_message = None
-		self.send_message = kwargs.pop('send_message', True)
-		super(SimpleBackend, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self._saved_message = None
+        self.send_message = kwargs.pop('send_message', True)
+        super(SimpleBackend, self).__init__(*args, **kwargs)
 
-	def send(self, message):
-		self._saved_message = message
-		return self.send_message
+    def send(self, message):
+        self._saved_message = message
+        return self.send_message
 
 
 class OurgoingTest(TestCase, CreateDataTest):
 
-	def setUp(self):
-		self.contact = self.create_contact()
-		self.backend = self.create_backend({'name': 'simple'})
-		self.connection = self.create_connection({'backend': self.backend,
-		                                          'contact': self.contact})
-		self.router = TestRouter()
+    def setUp(self):
+        self.contact = self.create_contact()
+        self.backend = self.create_backend({'name': 'simple'})
+        self.connection = self.create_connection({'backend': self.backend,
+                                                  'contact': self.contact})
+        self.router = TestRouter()
 
-	def test_outgoing(self):
-		"""
-		Router.outgoing should call backend.send() method
-		and set message.sent flag respectively
-		"""
-		backend = SimpleBackend(self.router, self.backend.name)
-		self.router.add_backend(self.backend.name, backend)
-		msg = OutgoingMessage(self.connection, 'hello!')
-		self.router.outgoing(msg)
-		self.assertTrue(msg.sent)
-		self.assertEqual(msg, backend._saved_message)
+    def test_outgoing(self):
+        """
+        Router.outgoing should call backend.send() method
+        and set message.sent flag respectively
+        """
+        backend = SimpleBackend(self.router, self.backend.name)
+        self.router.backends[self.backend.name] = backend
+        msg = OutgoingMessage(self.connection, 'hello!')
+        self.router.outgoing(msg)
+        self.assertTrue(msg.sent)
+        self.assertEqual(msg, backend._saved_message)
 
-	def test_outgoing_failure(self):
-		"""
-		Message sent flag should be false if backend.send() fails
-		"""
-		backend = SimpleBackend(self.router, self.backend.name, send_message=False)
-		self.router.add_backend(self.backend.name, backend)
-		msg = OutgoingMessage(self.connection, 'hello!')
-		self.router.outgoing(msg)
-		self.assertFalse(msg.sent)
+    def test_outgoing_failure(self):
+        """
+        Message sent flag should be false if backend.send() fails
+        """
+        backend = SimpleBackend(self.router, self.backend.name, send_message=False)
+        self.router.backends[self.backend.name] = backend
+        msg = OutgoingMessage(self.connection, 'hello!')
+        self.router.outgoing(msg)
+        self.assertFalse(msg.sent)
 
-	def test_handle_outgoing_with_connection(self):
-		"""
-		Router.handle_outgoing with a connection
-		"""
-		backend = SimpleBackend(self.router, self.backend.name)
-		self.router.add_backend(self.backend.name, backend)
-		self.router.handle_outgoing('hello!', connection=self.connection)
-		self.assertEqual('hello!', backend._saved_message.text)
-		self.assertEqual(self.connection, backend._saved_message.connection)
+    def test_handle_outgoing_with_connection(self):
+        """
+        Router.handle_outgoing with a connection
+        """
+        backend = SimpleBackend(self.router, self.backend.name)
+        self.router.backends[self.backend.name] = backend
+        self.router.handle_outgoing('hello!', connection=self.connection)
+        self.assertEqual('hello!', backend._saved_message.text)
+        self.assertEqual(self.connection, backend._saved_message.connection)
 
 
-class MessagingTest(CustomRouter, TestCase, CreateDataTest):
-	"""
-	Test rapidsms.contrib.messaging form and views
-	"""
+class MessagingTest(CustomRouter, CreateDataTest, TestCase):
+    """
+    Test rapidsms.contrib.messaging form and views
+    """
 
-	router_backends = {'simple': SimpleBackend}
+    backends = {'simple': {'ENGINE': SimpleBackend}}
 
-	def setUp(self):
-		super(MessagingTest, self).setUp()
-		self.contact = self.create_contact()
-		self.backend = self.create_backend({'name': 'simple'})
-		self.connection = self.create_connection({'backend': self.backend,
-		                                          'contact': self.contact})
+    def setUp(self):
+        self.contact = self.create_contact()
+        self.backend = self.create_backend({'name': 'simple'})
+        self.connection = self.create_connection({'backend': self.backend,
+                                                  'contact': self.contact})
 
-	def test_contacts_with_connection(self):
-		"""
-		Only contacts with connections are valid options
-		"""
-		connectionless_contact = self.create_contact()
-		data = {'text': 'hello!',
-		        'recipients': [self.contact.id, connectionless_contact.pk]}
-		form = MessageForm(data)
-		self.assertTrue('recipients' in form.errors)
+    def test_contacts_with_connection(self):
+        """
+        Only contacts with connections are valid options
+        """
+        connectionless_contact = self.create_contact()
+        data = {'text': 'hello!',
+                'recipients': [self.contact.id, connectionless_contact.pk]}
+        form = MessageForm(data)
+        self.assertTrue('recipients' in form.errors)
 
-	def test_valid_send_data(self):
-		"""
-		MessageForm.send should return successfully with valid data
-		"""
-		data = {'text': 'hello!',
-		        'recipients': [self.contact.id]}
-		form = MessageForm(data)
-		self.assertTrue(form.is_valid())
-		recipients = form.send()
-		self.assertTrue(self.contact in recipients)
+    def test_valid_send_data(self):
+        """
+        MessageForm.send should return successfully with valid data
+        """
+        data = {'text': 'hello!',
+                'recipients': [self.contact.id]}
+        form = MessageForm(data)
+        self.assertTrue(form.is_valid())
+        recipients = form.send()
+        self.assertTrue(self.contact in recipients)
 
-	def test_ajax_send_view(self):
-		"""
-		Test AJAX send view with valid data
-		"""
-		data = {'text': 'hello!',
-		        'recipients': [self.contact.id]}
-		response = self.client.post(reverse('send_message'), data)
-		self.assertEqual(response.status_code, 200)
+    def test_ajax_send_view(self):
+        """
+        Test AJAX send view with valid data
+        """
+        data = {'text': 'hello!',
+                'recipients': [self.contact.id]}
+        response = self.client.post(reverse('send_message'), data)
+        self.assertEqual(response.status_code, 200)
