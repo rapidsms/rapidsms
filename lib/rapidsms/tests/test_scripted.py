00c01cb4ccc987485ff31a8eec08b56267298c37
Colin Copeland <copelco@caktusgroup.com>
2011-10-16 17:15:02 -0400

    update EchoTest to use rapidsms.tests.scripted.TestScript

--- lib/rapidsms/tests/test_scripted.py
+++ lib/rapidsms/tests/test_scripted.py
@@ -1,13 +1,11 @@
-from django.test import TestCase
-
 from rapidsms.tests.harness import EchoApp
-from rapidsms.tests.harness.scripted import TestScript
+from rapidsms.tests.scripted import TestScript
 
 
-class EchoTest(TestScript, TestCase):
+class EchoTest(TestScript):
     apps = (EchoApp,)
 
-    def testRunScript (self):
+    def testRunScript(self):
         self.runScript("""
             2345678901 > echo?
             2345678901 < 2345678901: echo?
