4e2a21844746319e5683b42ec7700da6593deeca
Colin Copeland <copelco@caktusgroup.com>
2012-08-16 09:38:39 -0400

    use settings file for tox

--- run_tests.py
+++ run_tests.py
@@ -3,124 +3,53 @@
 
 import os
 import sys
-import optparse
-
-from django.conf import settings
-from django.core.management import call_command
 
 
-parser = optparse.OptionParser()
-opts, args = parser.parse_args()
-
-
-if not settings.configured:
-    jenkins = []
-    db_name = 'test_rapidsms'
-    db_engine = os.environ.get('DBENGINE', 'sqlite3')
-    if 'ci' in args:
-        db_name = "rapidsms_{0}".format(os.environ.get('TESTENV', db_name))
-    settings.configure(
-        TEST_RUNNER = "django_nose.NoseTestSuiteRunner",
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.{0}'.format(db_engine),
-                'NAME': '{0}.sqlite3'.format(db_name),
-            }
-        },
-        INSTALLED_BACKENDS = {
-            "message_tester": {
-                "ENGINE": "rapidsms.contrib.httptester.backend",
-            },
-        },
-        INSTALLED_APPS = [
-            "rapidsms",
-            # third party apps.
-            "django_nose",
-            "djtables",
-            # django contrib apps
-            "django.contrib.sites",
-            "django.contrib.auth",
-            "django.contrib.admin",
-            "django.contrib.sessions",
-            "django.contrib.contenttypes",
-            # rapidsms contrib apps.
-            "rapidsms.contrib.default",
-            "rapidsms.contrib.export",
-            "rapidsms.contrib.handlers",
-            "rapidsms.contrib.httptester",
-            "rapidsms.contrib.locations",
-            "rapidsms.contrib.messagelog",
-            "rapidsms.contrib.messaging",
-            "rapidsms.contrib.registration",
-            "rapidsms.contrib.echo",
-        ],
-        SITE_ID = 1,
-        SECRET_KEY = 'super-secret',
-        LOGIN_REDIRECT_URL = "/",
-        # Insecure, but fast for running tests
-        PASSWORD_HASHERS = (
-            'django.contrib.auth.hashers.MD5PasswordHasher',
-        ),
-        ROOT_URLCONF = "rapidsms.skeleton.project.urls",
-        RAPIDSMS_TABS = [
-            ("rapidsms.contrib.messagelog.views.message_log",       "Message Log"),
-            ("rapidsms.contrib.registration.views.registration",    "Registration"),
-            ("rapidsms.contrib.messaging.views.messaging",          "Messaging"),
-            ("rapidsms.contrib.locations.views.locations",          "Map"),
-            ("rapidsms.contrib.scheduler.views.index",              "Event Scheduler"),
-            ("rapidsms.contrib.httptester.views.generate_identity", "Message Tester"),
-        ],
-        # Only test these modules when using continuous integration ('ci' flag)
-        CI_APPS = [
-            # Root level modules
-            'rapidsms.forms',
-            'rapidsms.models',
-            'rapidsms.views',
-            # Sub folders
-            'rapidsms.apps',
-            'rapidsms.backends',
-            'rapidsms.management',
-            'rapidsms.messages',
-            'rapidsms.router',
-            'rapidsms.tests',
-            'rapidsms.utils',
-            # Contrib apps
-            'rapidsms.contrib.default',
-            'rapidsms.contrib.export',
-            'rapidsms.contrib.handlers',
-            'rapidsms.contrib.httptester',
-            'rapidsms.contrib.locations',
-            'rapidsms.contrib.messagelog',
-            'rapidsms.contrib.messaging',
-            'rapidsms.contrib.registration',
-            'rapidsms.contrib.echo',
-        ],
-    )
-
-
-def run_ci_tests():
-    cmd_flags = [
-        'with-xcoverage',
-        'cover-tests',
-        'noinput',
-    ]
-    # If settings.CI_APPS isn't set, default to testing all modules.
-    ci_apps = getattr(settings, 'CI_APPS', ['rapidsms'])
-    cmd_flags.extend(('cover-package=' + app for app in ci_apps))
-    named_flags = ' --'.join(cmd_flags)
-    os.system('django-admin.py test --{0}'.format(named_flags))
-
-
-def run_tests():
+def run_tests(options, ci=False):
+    from django.conf import settings
+    if ci:
+        settings.NOSE_ARGS = [
+            '--with-xcoverage',
+            '--cover-tests',
+            '--cover-package=rapidsms',
+        ]
     from django.test.utils import get_runner
     TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
+    test_runner = TestRunner(verbosity=int(options.verbosity),
+                             interactive=options.interactive, failfast=False)
     failures = test_runner.run_tests(['rapidsms', ])
     sys.exit(failures)
 
 
 if __name__ == '__main__':
-    if 'ci' in args:
-        run_ci_tests()
+    from optparse import OptionParser
+    usage = "%prog [options] [module module module ...]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-v', '--verbosity', action='store', dest='verbosity',
+                      default='1', type='choice', choices=['0', '1', '2', '3'],
+                      help='Verbosity level; 0=minimal output, 1=normal '
+                           'output, 2=all output')
+    parser.add_option('--noinput', action='store_false', dest='interactive',
+                      default=True,
+                      help='Tells Django to NOT prompt the user for input of '
+                           'any kind.')
+    parser.add_option('--settings',
+                      help='Python path to settings module, e.g. '
+                           '"myproject.settings". If this isn\'t provided, '
+                           'the DJANGO_SETTINGS_MODULE environment variable '
+                           'will be used.')
+    parser.add_option('--ci', action='store_true', dest='ci',
+                      default=False,
+                      help='Run tests with CI environment')
+    options, args = parser.parse_args()
+    if options.settings:
+        os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
+    elif "DJANGO_SETTINGS_MODULE" not in os.environ:
+        parser.error("DJANGO_SETTINGS_MODULE is not set in the environment. "
+                     "Set it or use --settings.")
     else:
-        run_tests()
+        options.settings = os.environ['DJANGO_SETTINGS_MODULE']
+    # I couldn't figure out how to get "CI" in as a command line argument,
+    # because nosetests would also interpret the argument.
+    ci = os.environ.get('CI', False)
+    run_tests(options, ci)
