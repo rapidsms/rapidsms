e6802a8ca8aa25e83417c8bb144c4f3711506486
Dan Poirier <poirier@pobox.com>
2012-08-29 13:27:00 -0400

    Fix new test, all tests pass under tox

--- lib/rapidsms/contrib/messagelog/tests.py
+++ lib/rapidsms/contrib/messagelog/tests.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-
-from rapidsms.conf import settings
+from django.test import TestCase
+from rapidsms.tests.harness.base import CreateDataTest
 import rapidsms.contrib.messagelog.app
 
 
-def test_messagelog():
-    app = rapidsms.contrib.messagelog.app.App()
-    # Invoke _log, make sure it doesn't blow up regardless of Django version
-    app._log('I', {}, "text")
+class MessageLogTest(CreateDataTest, TestCase):
+    def test_messagelog(self):
+        app = rapidsms.contrib.messagelog.app.App(None)
+        # Invoke _log, make sure it doesn't blow up regardless of Django version
+        contact = self.create_contact()
+        app._log('I', {'contact': contact}, "text")
