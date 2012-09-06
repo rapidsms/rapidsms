907e92f847fc6c91c470a77d21317fd2f2d2b652
Tobias McNulty <tobias@caktusgroup.com>
2012-09-05 22:56:06 -0400

    fix old call to handle_outgoing

--- lib/rapidsms/messages/outgoing.py
+++ lib/rapidsms/messages/outgoing.py
@@ -7,7 +7,7 @@ from django.utils.translation.trans_real import translation
 from .base import MessageBase
 from ..errors import NoRouterError
 from ..conf import settings
-from .router_api import handle_outgoing
+from rapidsms.router.api import send
 
 
 class OutgoingMessage(MessageBase):
@@ -75,5 +75,5 @@ class OutgoingMessage(MessageBase):
         the new interface for sending messages.
         """
         # TODO decide if this API is deprecated and add a deprecation warning if so
-        handle_outgoing(self.text, connection=self.connection)
+        send(self.text, connection=self.connection)
         self.sent = True
