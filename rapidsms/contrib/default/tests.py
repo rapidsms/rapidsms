236527f3bc5129815d5daa1212877f9b54830db7
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-01-11 14:40:38 -0500

    refs #218 - Use self.apps to set router apps in tests

--- rapidsms/contrib/default/tests.py
+++ rapidsms/contrib/default/tests.py
@@ -2,22 +2,34 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
+from django.conf import settings
 from django.test.utils import override_settings
 from rapidsms.tests.harness import RapidTest
+from rapidsms.tests.harness import EchoApp
 
 
-@override_settings(INSTALLED_APPS=['rapidsms.contrib.default'])
+class ReturningEchoApp(EchoApp):
+
+    def handle(self, message):
+        super(ReturningEchoApp, self).handle(message)
+        return True  # Return so that future apps don't handle the message
+
+
 class DefaultAppTest(RapidTest):
+    apps = ['rapidsms.contrib.default']
 
     def receive(self, text, connection=None, fields=None):
         connection = connection or self.lookup_connections('1112223333')[0]
         return super(DefaultAppTest, self).receive(text, connection, fields)
 
-    @override_settings(DEFAULT_RESPONSE='hello',
-                       INSTALLED_APPS=['rapidsms.contrib.echo',
-                                       'rapidsms.contrib.default'])
+    @override_settings(DEFAULT_RESPONSE='hello')
     def test_no_handle(self):
         """App should not respond to message if another has responded."""
+        # Add another app & reset the test router.
+        self.apps = [ReturningEchoApp, 'rapidsms.contrib.default']
+        self.set_router()
+        setattr(settings, 'RAPIDSMS_ROUTER', self.router_class)
+
         self.receive('ping')
         self.assertEqual(len(self.outbound), 1)
         self.assertNotEqual(self.outbound[0].text, 'hello')
