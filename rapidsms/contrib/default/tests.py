2db199d5dadfb7c5a51a216dc67a5d055e408dae
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-01-16 18:10:50 -0500

    refs #218 - Nitpicks

--- rapidsms/contrib/default/tests.py
+++ rapidsms/contrib/default/tests.py
@@ -3,8 +3,7 @@
 
 
 from django.conf import settings
-from rapidsms.tests.harness import RapidTest
-from rapidsms.tests.harness import EchoApp
+from rapidsms.tests.harness import EchoApp, RapidTest
 
 
 class ReturningEchoApp(EchoApp):
@@ -23,6 +22,8 @@ class DefaultAppTest(RapidTest):
         if hasattr(settings, 'PROJECT_NAME'):
             self._project_name = settings.PROJECT_NAME
 
+        self.connection = self.lookup_connections('1112223333')[0]
+
     def tearDown(self):
         if hasattr(self, '_default_response'):
             settings.DEFAULT_RESPONSE = self._default_response
@@ -33,32 +34,28 @@ class DefaultAppTest(RapidTest):
         elif hasattr(settings, 'PROJECT_NAME'):
             del settings.PROJECT_NAME
 
-    def receive(self, text, connection=None, fields=None):
-        connection = connection or self.lookup_connections('1112223333')[0]
-        return super(DefaultAppTest, self).receive(text, connection, fields)
-
     def test_no_handle(self):
         """App should not respond to message if another has responded."""
         # Add another app & reset the test router.
         self.apps = [ReturningEchoApp, 'rapidsms.contrib.default']
         self.set_router()
-        setattr(settings, 'RAPIDSMS_ROUTER', self.router_class)
+        settings.RAPIDSMS_ROUTER = self.router_class
 
         settings.DEFAULT_RESPONSE = 'hello'
-        self.receive('ping')
+        self.receive('ping', self.connection)
         self.assertEqual(len(self.outbound), 1)
         self.assertNotEqual(self.outbound[0].text, 'hello')
 
     def test_response_is_none(self):
         """App should not respond if DEFAULT_RESPONSE is None."""
         settings.DEFAULT_RESPONSE = None
-        self.receive('asdf')
+        self.receive('asdf', self.connection)
         self.assertEqual(len(self.outbound), 0)
 
     def test_response_no_project_name(self):
         """App should respond with exact string if project_name is not used."""
         settings.DEFAULT_RESPONSE = 'hello'
-        self.receive('asdf')
+        self.receive('asdf', self.connection)
         self.assertEqual(len(self.outbound), 1)
         self.assertEqual(self.outbound[0].text, 'hello')
 
@@ -66,6 +63,6 @@ class DefaultAppTest(RapidTest):
         """App should replace project_name in response if it is present."""
         settings.DEFAULT_RESPONSE = 'hello %(project_name)s'
         settings.PROJECT_NAME = 'world'
-        self.receive('asdf')
+        self.receive('asdf', self.connection)
         self.assertEqual(len(self.outbound), 1)
         self.assertEqual(self.outbound[0].text, 'hello world')
