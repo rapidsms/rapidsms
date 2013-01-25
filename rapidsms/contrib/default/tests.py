32149ec6a4136412e5529475515765436f6d65c1
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-01-25 11:41:46 -0500

    Use override_settings if it is available

--- rapidsms/contrib/default/tests.py
+++ rapidsms/contrib/default/tests.py
@@ -2,7 +2,12 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 
-from rapidsms.tests.harness import EchoApp, RapidTest, setting
+from rapidsms.tests.harness import EchoApp, RapidTest
+
+try:
+    from django.test.utils import override_settings
+except ImportError:
+    from rapidsms.tests.harness import setting as override_settings
 
 
 class ReturningEchoApp(EchoApp):
@@ -23,7 +28,7 @@ class DefaultAppTest(RapidTest):
         # Add another app & reset the test router.
         self.apps = [ReturningEchoApp, 'rapidsms.contrib.default']
         self.set_router()
-        with setting(RAPIDSMS_ROUTER=self.router_class,
+        with override_settings(RAPIDSMS_ROUTER=self.router_class,
                      DEFAULT_RESPONSE='hello'):
             self.receive('ping', self.connection)
             self.assertEqual(len(self.outbound), 1)
@@ -31,20 +36,20 @@ class DefaultAppTest(RapidTest):
 
     def test_response_is_none(self):
         """App should not respond if DEFAULT_RESPONSE is None."""
-        with setting(DEFAULT_RESPONSE=None):
+        with override_settings(DEFAULT_RESPONSE=None):
             self.receive('asdf', self.connection)
             self.assertEqual(len(self.outbound), 0)
 
     def test_response_no_project_name(self):
         """App should respond with exact string if project_name is not used."""
-        with setting(DEFAULT_RESPONSE='hello'):
+        with override_settings(DEFAULT_RESPONSE='hello'):
             self.receive('asdf', self.connection)
             self.assertEqual(len(self.outbound), 1)
             self.assertEqual(self.outbound[0].text, 'hello')
 
     def test_response_with_project_name(self):
         """App should replace project_name in response if it is present."""
-        with setting(DEFAULT_RESPONSE='hello %(project_name)s',
+        with override_settings(DEFAULT_RESPONSE='hello %(project_name)s',
                      PROJECT_NAME='world'):
             self.receive('asdf', self.connection)
             self.assertEqual(len(self.outbound), 1)
