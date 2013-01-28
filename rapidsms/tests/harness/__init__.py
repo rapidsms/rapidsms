e400d09f2dd029561da665d9d7c65f42fc5afce0
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-01-28 09:53:09 -0500

    Context manager now deletes settings that did not previously exist The default value, None, was a valid (but unintended) setting for some settings.

--- rapidsms/tests/harness/__init__.py
+++ rapidsms/tests/harness/__init__.py
@@ -42,14 +42,16 @@ class setting(object):
     def __init__(self, **kwargs):
         self.settings = kwargs
         self.saved_settings = {}
-        self.default_value = None
 
     def __enter__(self):
         for k, v in self.settings.items():
-            self.saved_settings[k] = getattr(settings, k, self.default_value)
+            if hasattr(settings, k):
+                self.saved_settings[k] = getattr(settings, k)
             setattr(settings, k, v)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        for k, v in self.saved_settings.items():
-            if v != self.default_value:
-                setattr(settings, k, v)
+        for k in self.settings.keys():
+            if k in self.saved_settings:
+                setattr(settings, k, self.saved_settings[k])
+            else:
+                delattr(settings, k)
