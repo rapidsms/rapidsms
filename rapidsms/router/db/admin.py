d1549b2ae79def8951ab4e647d2d26510125479f
Colin Copeland <copelco@caktusgroup.com>
2013-02-03 21:29:14 -0500

    add message to transmission list; search by message text as well

--- rapidsms/router/db/admin.py
+++ rapidsms/router/db/admin.py
@@ -11,11 +11,12 @@ class MessageAdmin(admin.ModelAdmin):
 
 
 class TransmissionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date', 'status', 'connection', 'updated', 'sent',
-                    'delivered')
+    list_display = ('id', 'date', 'message', 'status', 'connection', 'updated',
+                    'sent', 'delivered')
     ordering = ('-updated',)
     list_filter = ('status',)
     raw_id_fields = ('message', 'connection',)
+    search_fields = ('message__text',)
 
 
 admin.site.register(Message, MessageAdmin)
