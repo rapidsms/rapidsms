14b11d0810aaadcb7388552934cc3b753400693a
Alexander Lemann <lemanal@caktusgroup.com>
2013-02-18 12:52:02 -0600

    Use csv module for bulk registration.
    Add error reporting

--- rapidsms/contrib/registration/views.py
+++ rapidsms/contrib/registration/views.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+import csv
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 from django.db import transaction
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from rapidsms.models import Contact, Connection, Backend
 from rapidsms.contrib.registration.tables import ContactTable
@@ -11,8 +12,9 @@ from rapidsms.contrib.registration.forms import BulkRegistrationForm, ContactFor
 
 
 def registration(request):
-    return render(request, "registration/dashboard.html",
-        {"contacts_table": ContactTable(Contact.objects.all(), request=request)},)
+    return render(request, "registration/dashboard.html", {
+        "contacts_table": ContactTable(Contact.objects.all(), request=request)
+        })
 
 
 def contact_add(request):
@@ -28,38 +30,40 @@ def contact_add(request):
         })
 
 
+@transaction.commit_on_success
 def contact_bulk_add(request):
-    bulk_form = BulkRegistrationForm()
-    if "bulk" in request.FILES:
-    # TODO use csv module
-    #reader = csv.reader(open(req.FILES["bulk"].read(), "rb"))
-    #for row in reader:
-        for line in request.FILES["bulk"]:
-            line_list = line.split(',')
-            name = line_list[0].strip()
-            backend_name = line_list[1].strip()
-            identity = line_list[2].strip()
+    bulk_form = BulkRegistrationForm(data=request.POST)
 
+    if request.method == "POST" and "bulk" in request.FILES:
+        connections, contacts = [], []
+        reader = csv.reader(request.FILES["bulk"], quoting=csv.QUOTE_NONE, skipinitialspace=True)
+        for i, row in enumerate(reader, start=1):
+            try:
+                name, backend_name, identity = row
+            except:
+                return render(request, 'registration/bulk_form.html', {
+                    "bulk_form": bulk_form,
+                    "csv_errors": "Could not unpack line " + str(i),
+                })
             contact = Contact(name=name)
-            contact.save()
-            # TODO deal with errors!
-            backend = Backend.objects.get(name=backend_name)
-
-            connection = Connection(backend=backend, identity=identity,
-                                    contact=contact)
-            connection.save()
+            contacts.append(contact)
+            try:
+                backend = Backend.objects.get(name=backend_name)
+            except:
+                return render(request, 'registration/bulk_form.html', {
+                    "bulk_form": bulk_form,
+                    "csv_errors": "Could not find Backend.  Line: " + str(i),
+                })
+            connections.append(Connection(backend=backend, identity=identity,
+                                        contact=contact))
         return HttpResponseRedirect(reverse(registration))
-
     return render(request, 'registration/bulk_form.html', {
         "bulk_form": bulk_form,
         })
 
 
-@transaction.commit_on_success
-def contact_edit(request, pk=None):
-    contact = None
-    if pk is not None:
-        contact = get_object_or_404(Contact, pk=pk)
+def contact_edit(request, pk):
+    contact = get_object_or_404(Connection, pk=pk)
 
     if request.method == "POST":
         if request.POST["submit"] == "Delete Contact":
@@ -67,15 +71,12 @@ def contact_edit(request, pk=None):
             return HttpResponseRedirect(
                 reverse(registration))
         else:
-            contact_form = ContactForm(
-                instance=contact,
-                data=request.POST)
+            contact_form = Connection(instance=contact, data=request.POST)
 
             if contact_form.is_valid():
                 contact = contact_form.save()
                 return HttpResponseRedirect(
                     reverse(registration))
-    return render(request, "registration/dashboard.html", {
+    return render(request, "registration/contact_form.html", {
             "contact": contact
-        },
-    )
+        })
