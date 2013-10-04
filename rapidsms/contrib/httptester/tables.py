84d31e9c51812f092e613a21884273a341c973e0
ewheeler <evanmwheeler@gmail.com>
2013-10-04 17:37:55 -0400

    add styles to messagelog and httptester tables

--- rapidsms/contrib/httptester/tables.py
+++ rapidsms/contrib/httptester/tables.py
@@ -11,17 +11,19 @@ from rapidsms.backends.database.models import INCOMING, BackendMessage
 class MessageTable(tables.Table):
 
     def render_identity(self, record):
-        # Render the phone number with a double arrow pointing to it
+        # Render the phone number with an  arrow pointing to it
         # or away from it, depending on whether the message was going
         # out or coming in.
         if record.direction == INCOMING:
-            return mark_safe(record.identity + "&raquo;")
+            return mark_safe(record.identity + "&nbsp;"
+                             "<span class='arrow-left'>&larr;</span>")
         else:
-            return mark_safe(record.identity + "&laquo;")
+            return mark_safe(record.identity + "&nbsp;"
+                             "<span class='arrow-right'>&rarr;</span>")
 
     class Meta:
         model = BackendMessage
         sequence = ('date', 'identity', 'text')
         exclude = ('id', 'direction', 'name', 'message_id', 'external_id')
         order_by = ('-date', )
-        attrs = {'id': 'log'}
+        attrs = {'id': 'log', 'class': 'messages'}
--- rapidsms/contrib/messagelog/tables.py
+++ rapidsms/contrib/messagelog/tables.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+from django.utils.safestring import mark_safe
 import django_tables2 as tables
 from rapidsms.contrib.messagelog.models import Message
+from rapidsms.backends.database.models import INCOMING
 
 
 class MessageTable(tables.Table):
+    def render_direction(self, record):
+        if record.direction == INCOMING:
+            return mark_safe("<span class='arrow-left'>&larr;</span>")
+        else:
+            return mark_safe("<span class='arrow-right'>&rarr;</span>")
 
     class Meta:
         model = Message
