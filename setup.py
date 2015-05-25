68dcdd7826ae37d6887affeaeb27349b494fb1d5
Vinod Kurup <vinod@kurup.com>
2015-05-25 15:44:10 -0400

    New version of django-tables2 doesn't support Py26

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 
     install_requires=[
         "requests>=1.2.0",
-        "django-tables2>=0.14.0",
+        "django-tables2>=0.14.0,<1.0.0",
         "djappsettings>=0.3.0",
         "django-selectable>=0.7.0",
     ],
