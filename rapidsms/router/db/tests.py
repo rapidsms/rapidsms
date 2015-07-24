b294c5b08e1abf284d83cc6eab14b52446b6b2b4
Vinod Kurup <vinod@kurup.com>
2015-07-24 12:08:44 -0400

    Refactor tests to make backend more configurable

--- rapidsms/router/db/tests.py
+++ rapidsms/router/db/tests.py
@@ -192,12 +192,13 @@ class DatabaseRouterSendTest(harness.DatabaseBackendMixin, TestCase):
 
     router_class = 'rapidsms.router.db.DatabaseRouter'
 
-    def create_trans(self, s1='Q', s2='Q'):
+    def create_trans(self, s1='Q', s2='Q', backend=None):
+        backend = backend or self.backend
         Connection.objects.bulk_create((
-            Connection(identity='1111111111', backend=self.backend),
-            Connection(identity='2222222222', backend=self.backend),
-            Connection(identity='3333333333', backend=self.backend),
-            Connection(identity='4444444444', backend=self.backend),
+            Connection(identity='1111111111', backend=backend),
+            Connection(identity='2222222222', backend=backend),
+            Connection(identity='3333333333', backend=backend),
+            Connection(identity='4444444444', backend=backend),
         ))
         dbm = Message.objects.create(text="test", direction="O")
         for connection in Connection.objects.order_by('id')[:2]:
@@ -207,11 +208,10 @@ class DatabaseRouterSendTest(harness.DatabaseBackendMixin, TestCase):
         ids = dbm.transmissions.order_by('id').values_list('id', flat=True)
         trans1 = dbm.transmissions.filter(id__in=ids[:2])
         trans2 = dbm.transmissions.filter(id__in=ids[2:])
-        return self.backend, dbm, trans1, trans2
+        return dbm, trans1, trans2
 
     def create_many_transmissions(self, num, backend=None):
-        if not backend:
-            backend = self.backend
+        backend = backend or self.backend
         # Create a message that will be sent to many connections
         message = Message.objects.create(text="test", direction="O")
         for i in range(num):
@@ -222,31 +222,31 @@ class DatabaseRouterSendTest(harness.DatabaseBackendMixin, TestCase):
     def test_send_successful_status(self):
         """Transmissions should be marked with S if no errors occured."""
         # create 2 batches (queued, queued)
-        backend, dbm, t1, t2 = self.create_trans(s1='Q', s2='Q')
-        send_transmissions(backend.pk, dbm.pk, t1.values_list('id', flat=True))
+        dbm, t1, t2 = self.create_trans(s1='Q', s2='Q')
+        send_transmissions(self.backend.pk, dbm.pk, t1.values_list('id', flat=True))
         status = t1.values_list('status', flat=True).distinct()[0]
         self.assertEqual('S', status)
 
     def test_send_successful_message_status(self):
         """Message object should be updated if all transmissions were sent."""
         # create 2 batches (sent, queued)
-        backend, dbm, t1, t2 = self.create_trans(s1='S', s2='Q')
-        send_transmissions(backend.pk, dbm.pk, t2.values_list('id', flat=True))
+        dbm, t1, t2 = self.create_trans(s1='S', s2='Q')
+        send_transmissions(self.backend.pk, dbm.pk, t2.values_list('id', flat=True))
         dbm = Message.objects.all()[0]
         self.assertEqual('S', dbm.status)
 
     def test_send_successful_message_status_previous_error(self):
         """Message should be marked E even if current batch sends."""
         # create 2 batches (error, queued)
-        backend, dbm, t1, t2 = self.create_trans(s1='E', s2='Q')
-        send_transmissions(backend.pk, dbm.pk, t2.values_list('id', flat=True))
+        dbm, t1, t2 = self.create_trans(s1='E', s2='Q')
+        send_transmissions(self.backend.pk, dbm.pk, t2.values_list('id', flat=True))
         dbm = Message.objects.all()[0]
         self.assertEqual('E', dbm.status)
 
     def test_group_transmissions(self):
         """Transmissions should be grouped by batch_size."""
         # create 2 batches (queued, queued)
-        backend, dbm, t1, t2 = self.create_trans(s1='Q', s2='Q')
+        dbm, t1, t2 = self.create_trans(s1='Q', s2='Q')
         router = DatabaseRouter()
         trans = list(router.group_transmissions(Transmission.objects.all(),
                                                 batch_size=2))
@@ -296,9 +296,9 @@ class DatabaseRouterSendTest(harness.DatabaseBackendMixin, TestCase):
         """If a transmission has already been sent, don't resend it.
         (This may occur during a retry, when some of the messages were sent and some failed)."""
         # create 2 batches (sent, queued)
-        backend, dbm, t1, t2 = self.create_trans(s1='S', s2='Q')
+        dbm, t1, t2 = self.create_trans(s1='S', s2='Q')
         both_transmisssion_sets = list(t1.values_list('id', flat=True)) + list(t2.values_list('id', flat=True))
-        send_transmissions(backend.pk, dbm.pk, both_transmisssion_sets)
+        send_transmissions(self.backend.pk, dbm.pk, both_transmisssion_sets)
         dbm = Message.objects.get()
         self.assertEqual('S', dbm.status)
         # only 2 messages should be sent, both from t2
@@ -309,9 +309,9 @@ class DatabaseRouterSendTest(harness.DatabaseBackendMixin, TestCase):
         )
 
     def test_all_transmissions_set_to_E_if_backend_sending_error(self):
-        backend, dbm, t1, t2 = self.create_trans(s1='Q', s2='Q')
         error_backend = RaisesBackend(self.get_router(), 'error_backend')
         self.backends['error_backend'] = {'ENGINE': RaisesBackend}
+        dbm, t1, t2 = self.create_trans(s1='Q', s2='Q', backend=error_backend.model)
         both_transmisssion_sets = list(t1.values_list('id', flat=True)) + list(t2.values_list('id', flat=True))
         with override_settings(INSTALLED_BACKENDS=self.backends):
             with self.assertRaises(MessageSendingError):
@@ -321,11 +321,12 @@ class DatabaseRouterSendTest(harness.DatabaseBackendMixin, TestCase):
         self.assertEqual(4, errored.count())
 
     def test_only_failed_transmissions_set_to_E(self):
-        # FailedIdentitiesBackend will fail any messages to an identity with a '1' in it
-        # create_trans creates 4 identities, only one of which has a '1' in it
-        backend, dbm, t1, t2 = self.create_trans(s1='Q', s2='Q')
+        # FailedIdentitiesBackend will fail any messages to an identity with a '1' in it.
+        # create_trans creates 4 identities, only one of which has a '1' in it, so we expect
+        # one failure.
         error_backend = FailedIdentitiesBackend(self.get_router(), 'error_backend')
         self.backends['error_backend'] = {'ENGINE': FailedIdentitiesBackend}
+        dbm, t1, t2 = self.create_trans(s1='Q', s2='Q', backend=error_backend.model)
         both_transmisssion_sets = list(t1.values_list('id', flat=True)) + list(t2.values_list('id', flat=True))
         with override_settings(INSTALLED_BACKENDS=self.backends):
             with self.assertRaises(MessageSendingError):
