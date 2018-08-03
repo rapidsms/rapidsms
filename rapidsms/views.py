88be5325c3d006b822c8d8ee2db4512ea538c800
reginafcompton <reginafcompton@gmail.com>
2018-08-03 10:57:37 -0500

    Use render, rather than render_to_response

--- rapidsms/views.py
+++ rapidsms/views.py
@@ -4,17 +4,16 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login as django_login
 from django.contrib.auth.views import logout as django_logout
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.views.decorators.http import require_GET
 
 
 @login_required
 @require_GET
-def dashboard(req):
-    return render_to_response(
-        "dashboard.html",
-        context_instance=RequestContext(req))
+def dashboard(request):
+    return render(request,
+        "dashboard.html")
 
 
 def login(req, template_name="rapidsms/login.html"):
