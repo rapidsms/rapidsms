9e1ec758686c5b3a2023616d0ebf292bf8dff24c
Aristotel Fani <afani1997@gmail.com>
2023-07-19 10:17:52 -0400

    Run django-upgrade to update framework changes for django 4.2

--- rapidsms/backends/kannel/admin.py
+++ rapidsms/backends/kannel/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from rapidsms.backends.kannel.models import DeliveryReport
 
 
+@admin.register(DeliveryReport)
 class DeliveryReportAdmin(admin.ModelAdmin):
     search_fields = ('text', 'sms_id', 'smsc')
     list_display = ('id', 'message_id', 'date', 'identity', 'status',
                     'status_text', 'smsc', 'date_sent')
     list_filter = ('smsc', 'status')
     ordering = ('-date',)
-
-
-admin.site.register(DeliveryReport, DeliveryReportAdmin)
--- rapidsms/router/db/admin.py
+++ rapidsms/router/db/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from rapidsms.router.db.models import Message, Transmission
 
 
+@admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     search_fields = ('text',)
     list_display = ('id', 'date', 'direction', 'text', 'status', 'updated',
@@ -10,6 +11,7 @@ class MessageAdmin(admin.ModelAdmin):
     ordering = ('-updated',)
 
 
+@admin.register(Transmission)
 class TransmissionAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'message', 'status', 'connection', 'updated',
                     'sent', 'delivered')
@@ -17,7 +19,3 @@ class TransmissionAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     raw_id_fields = ('message', 'connection',)
     search_fields = ('message__text',)
-
-
-admin.site.register(Message, MessageAdmin)
-admin.site.register(Transmission, TransmissionAdmin)
